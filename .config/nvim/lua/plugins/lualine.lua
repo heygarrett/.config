c3b6d3585cd9efdb0f10531ebf37343ffc8a3cc8
Garrett Oreilly <hey@heygarrett.com>
2021-09-06 15:35:26 -0400

    Configuring lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,10 +1,21 @@
+local function filepath()
+	if vim.o.filetype == 'netrw' then
+		return vim.fn.getcwd()
+	elseif vim.fn.expand('%:F') ~= '' then
+		return vim.fn.expand('%:F') .. ' ' .. vim.fn.expand('%m')
+	else
+		return '[No Name]'
+	end
+end
+
 return require('lualine').setup {
 	options = {
+		icons_enabled = false,
 		theme = 'dracula',
-		section_separators = {' ', ' '},
-		component_separators = {'|', '|'}
+		component_separators = {'|', '|'},
+		section_separators = {'', ''}
 	},
 	sections = {
-		lualine_a = {'mode'}
+		lualine_c = {filepath}
 	}
 }
