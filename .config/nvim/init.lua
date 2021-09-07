12be2a07121f1d793a514cf7d1556e006a004010
Garrett Oreilly <hey@heygarrett.com>
2021-09-07 09:14:15 -0400

    Cleaning up neovim settings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,25 +7,23 @@ require('plugins/indent-blankline')
 vim.cmd('source $HOME/.config/nvim/viml/autocmds.vim')
 
 vim.g.python3_host_prog = vim.env.HOME .. "/.local/venvs/nvim/bin/python"
+vim.api.nvim_set_keymap('n', 'j', 'gj', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'k', 'gk', { noremap = true, silent = true })
+vim.o.hidden = true -- soon to be default (merged into master)
 vim.g.netrw_liststyle = 3
 vim.o.cmdheight = 2
-vim.o.confirm = true
-vim.o.mouse = 'a'
-vim.o.clipboard = 'unnamedplus'
+vim.o.scrolloff = 3
+vim.o.number = true
+vim.o.cursorline = true
 vim.o.showmatch = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
-vim.o.cursorline = true
-vim.o.number = true
-vim.o.scrolloff = 3
 vim.o.linebreak = true
 vim.o.list = true
+vim.o.confirm = true
 vim.o.splitbelow = true
 vim.o.splitright = true
-vim.o.hidden = true
-vim.o.foldmethod = 'indent'
 vim.o.foldenable = false
-vim.o.history = 1000
-vim.o.undolevels = 1000
-vim.api.nvim_set_keymap('n', 'j', 'gj', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'k', 'gk', { noremap = true, silent = true })
+vim.o.foldmethod = 'indent'
+vim.o.clipboard = 'unnamedplus'
+vim.o.mouse = 'a'
