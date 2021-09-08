575fd229edca5314eaf3f844f1a39ac5fdc4bd56
Garrett Oreilly <hey@heygarrett.com>
2021-09-07 20:12:48 -0400

    Swapped vim.o for vim.opt

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,21 +9,22 @@ vim.cmd('source $HOME/.config/nvim/viml/autocmds.vim')
 vim.g.python3_host_prog = vim.env.HOME .. "/.local/venvs/nvim/bin/python"
 vim.api.nvim_set_keymap('n', 'j', 'gj', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'k', 'gk', { noremap = true, silent = true })
-vim.o.hidden = true -- soon to be default (merged into master)
 vim.g.netrw_liststyle = 3
-vim.o.cmdheight = 2
-vim.o.scrolloff = 3
-vim.o.number = true
-vim.o.cursorline = true
-vim.o.showmatch = true
-vim.o.ignorecase = true
-vim.o.smartcase = true
-vim.o.linebreak = true
-vim.o.list = true
-vim.o.confirm = true
-vim.o.splitbelow = true
-vim.o.splitright = true
-vim.o.foldenable = false
-vim.o.foldmethod = 'indent'
-vim.o.clipboard = 'unnamedplus'
-vim.o.mouse = 'a'
+vim.opt.hidden = true -- soon to be default (merged into master)
+vim.opt.path:append({'**'})
+vim.opt.cmdheight = 2
+vim.opt.scrolloff = 3
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.showmatch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.linebreak = true
+vim.opt.list = true
+vim.opt.confirm = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.foldenable = false
+vim.opt.foldmethod = 'indent'
+vim.opt.clipboard:append({'unnamedplus'})
+vim.opt.mouse = 'a'
