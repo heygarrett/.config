662e547bee8fe657b1c669faf1eb13ef23ad8f1b
Garrett Oreilly <hey@heygarrett.com>
2021-09-09 22:28:14 -0400

    Added lua linting with luacheck

--- .config/nvim/lua/plugins/nvim-lint.lua
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -2,6 +2,8 @@ local lint = require('lint')
 lint.linters_by_ft = {
 	javascript = {'eslint'},
 	typescript = {'eslint'},
+	-- luarocks install luacheck lanes
+	lua = {'luacheck'},
 }
 -- lint.linters.eslint.cmd = './node_modules/.bin/eslint'
 
