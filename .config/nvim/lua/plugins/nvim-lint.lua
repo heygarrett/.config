3f65d8b929dec2629542cdcf125949c8ed4a67ef
Garrett Oreilly <hey@heygarrett.com>
2021-09-09 23:31:21 -0400

    Removed manual lint command

--- .config/nvim/lua/plugins/nvim-lint.lua
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -8,4 +8,3 @@ lint.linters_by_ft = {
 -- lint.linters.eslint.cmd = './node_modules/.bin/eslint'
 
 vim.cmd([[autocmd BufEnter,InsertLeave * lua require('lint').try_lint()]])
-vim.cmd([[command! TryLint lua require('lint').try_lint()]])
