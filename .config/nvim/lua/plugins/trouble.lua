67d32d3e563e344c7ade0b73be3bf0e4b95e4e2e
Garrett Oreilly <hey@heygarrett.com>
2021-09-13 02:37:48 -0400

    Trying out some Trouble keymaps

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -1,5 +1,20 @@
+vim.api.nvim_set_keymap("n", "<leader>td", "<cmd>Trouble lsp_document_diagnostics<cr>",
+  {silent = true, noremap = true}
+)
+vim.api.nvim_set_keymap("n", "<leader>tw", "<cmd>Trouble lsp_workspace_diagnostics<cr>",
+  {silent = true, noremap = true}
+)
+vim.api.nvim_set_keymap("n", "<leader>gd", "<cmd>Trouble lsp_definitions<cr>",
+  {silent = true, noremap = true}
+)
+vim.api.nvim_set_keymap("n", "<leader>gr", "<cmd>Trouble lsp_references<cr>",
+  {silent = true, noremap = true}
+)
+vim.api.nvim_set_keymap("n", "<leader>gi", "<cmd>Trouble lsp_implementations<cr>",
+  {silent = true, noremap = true}
+)
+
 return require("trouble").setup {
 	icons = false,
 	use_lsp_diagnostic_signs = true
-	-- TODO migrate keymappings from LSP config
 }
