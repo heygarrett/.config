578a0ab3994c3eb6315492f3ddfd1196787e91bd
Garrett Oreilly <hey@heygarrett.com>
2021-09-21 22:40:37 -0400

    Removed snippets built-in to coq

--- .config/nvim/lua/plugins/coq.lua
+++ .config/nvim/lua/plugins/coq.lua
@@ -2,7 +2,8 @@ vim.g.coq_settings = {
 	auto_start = true,
 	clients = {
 		['tmux.enabled'] = false,
-		['snippets.enabled'] = false
+		['snippets.enabled'] = false,
+		['snippets.warn'] = {},
 	},
 	['keymap.jump_to_mark'] = '<c-n>',
 }
