d42b933647f659f8b652c103534eef94ca2de563
Garrett Oreilly <hey@heygarrett.com>
2021-09-23 13:19:34 -0400

    Tidying Telescope commands

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,9 +1,11 @@
 local map = vim.api.nvim_set_keymap
-map('n', '<leader>ff', '<cmd>Telescope find_files<cr>', {noremap = true, silent = true})
-map('n', '<leader>ft', '<cmd>Telescope git_files<cr>', {noremap = true, silent = true})
-map('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', {noremap = true, silent = true})
-map('n', '<leader>fb', '<cmd>Telescope buffers<cr>', {noremap = true, silent = true})
-map('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', {noremap = true, silent = true})
+local opts = {noremap = true, silent = true}
+
+map('n', '<leader>ff', '<cmd>Telescope find_files<cr>', opts)
+map('n', '<leader>ft', '<cmd>Telescope git_files<cr>', opts)
+map('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', opts)
+map('n', '<leader>fb', '<cmd>Telescope buffers<cr>', opts)
+map('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', opts)
 
 local telescope = require('telescope')
 telescope.load_extension('fzf')
