e28d443f36b90eeaaf557157719e12eafefe2250
Garrett Oreilly <hey@heygarrett.com>
2021-09-23 00:27:32 -0400

    Playing with Telescope shortcuts

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,11 +1,13 @@
 local map = vim.api.nvim_set_keymap
-map('n', '<leader>ff', '<cmd>lua require(\'telescope.builtin\').find_files()<cr>', {noremap = true, silent = true})
-map('n', '<leader>fg', '<cmd>lua require(\'telescope.builtin\').live_grep()<cr>', {noremap = true, silent = true})
-map('n', '<leader>fb', '<cmd>lua require(\'telescope.builtin\').buffers()<cr>', {noremap = true, silent = true})
-map('n', '<leader>fh', '<cmd>lua require(\'telescope.builtin\').help_tags()<cr>', {noremap = true, silent = true})
+map('n', '<leader>ff', '<cmd>Telescope find_files<cr>', {noremap = true, silent = true})
+map('n', '<leader>ft', '<cmd>Telescope git_files<cr>', {noremap = true, silent = true})
+map('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', {noremap = true, silent = true})
+map('n', '<leader>fb', '<cmd>Telescope buffers<cr>', {noremap = true, silent = true})
+map('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', {noremap = true, silent = true})
 
-require('telescope').setup {
+local telescope = require('telescope')
+telescope.load_extension('fzf')
+telescope.setup {
 	defaults = {}
 }
 
-require('telescope').load_extension('fzf')
