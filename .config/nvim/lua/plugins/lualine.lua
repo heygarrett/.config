7f70acb60f75189bc655a8874fe083af37de358a
Garrett Oreilly <hey@heygarrett.com>
2021-09-28 13:16:24 -0400

    Tidying status line config

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,35 +1,19 @@
 vim.opt.laststatus = 2
 vim.opt.showmode = false
 
-local function check_modified()
-	if vim.opt.readonly:get() == true then
-		return '[-]'
-	elseif vim.opt.modified:get() == true then
-		return '[+]'
-	else
-		return ''
-	end
-end
-
-local function filepath()
-	local path
-	if vim.opt.filetype:get() == 'netrw' then
-		path = vim.fn.getcwd()
-	elseif vim.fn.expand('%:p:.') ~= '' then
-		path = vim.fn.expand('%:p:.')
-	else
-		path = '[No Name]'
-	end
-	return path .. check_modified()
-end
-
 local function round(p)
 	return p % 1 >= .5 and math.ceil(p) or math.floor(p)
 end
 
 local function progress()
-	local p = vim.fn.line('.') * 100 / vim.fn.line('$')
-	return tostring(round(p)) .. '%%'
+	if vim.fn.line('.') == 1 then
+		return 'top'
+	elseif vim.fn.line('.') == vim.fn.line('$') then
+		return 'bot'
+	else
+		local p = vim.fn.line('.') / vim.fn.line('$') * 100
+		return tostring(round(p)) .. '%%'
+	end
 end
 
 return require('lualine').setup {
@@ -40,7 +24,9 @@ return require('lualine').setup {
 		section_separators = {'', ''}
 	},
 	sections = {
-		lualine_c = {filepath},
-		lualine_y = {progress}
-	}
+		lualine_c = {
+			{'filename', file_status = true, path = 1}
+		},
+		lualine_y = {progress},
+	},
 }
