55ffadcf3cb8ea6abc58aa6e418a3822c1f55d8f
Garrett Oreilly <hey@heygarrett.com>
2021-09-29 17:58:12 -0400

    Snippets plugin removed

--- .config/nvim/lua/plugins/coq.lua
+++ .config/nvim/lua/plugins/coq.lua
@@ -2,7 +2,6 @@ vim.g.coq_settings = {
 	auto_start = true,
 	clients = {
 		['tmux.enabled'] = false,
-		['snippets.enabled'] = false,
 		['snippets.warn'] = {},
 	},
 	['keymap.jump_to_mark'] = '<c-n>',
