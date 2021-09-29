984d2204772e987c6bb004534670a99c1a608c50
Garrett Oreilly <hey@heygarrett.com>
2021-09-29 12:52:17 -0400

    Sorting init.lua alphabetically, because why not?

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,11 +1,11 @@
-require('plugins/packer')
-require('plugins/lspconfig')
 require('plugins/efm-langserver')
-require('utils/diagnostic-signs')
-require('plugins/lualine')
 require('plugins/indexed-search')
+require('plugins/lspconfig')
+require('plugins/lualine')
+require('plugins/packer')
 require('plugins/telescope')
 require('plugins/treesitter')
+require('utils/diagnostic-signs')
 
 -- Colorscheme
 vim.api.nvim_command('colorscheme dracula_pro')
@@ -18,34 +18,34 @@ map('n', 'k', 'gk', opts)
 map('n', '<s-tab>', '<c-o>', opts)
 
 -- Options
-vim.g.python3_host_prog = vim.env.HOME .. '/.local/venvs/nvim/bin/python'
-vim.g.netrw_liststyle = 3
 vim.g.netrw_banner = 0
-vim.opt.hidden = true
-vim.opt.path:append('**')
-vim.opt.termguicolors = true
+vim.g.netrw_liststyle = 3
+vim.g.python3_host_prog = vim.env.HOME .. '/.local/venvs/nvim/bin/python'
+vim.opt.clipboard:append('unnamedplus')
 vim.opt.cmdheight = 2
-vim.opt.scrolloff = 3
-vim.opt.number = true
-vim.opt.cursorline = true
-vim.opt.showmatch = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.linebreak = true
-vim.opt.list = true
 vim.opt.confirm = true
-vim.opt.splitbelow = true
-vim.opt.splitright = true
+vim.opt.cursorline = true
 vim.opt.foldenable = false
 vim.opt.foldmethod = 'indent'
-vim.opt.clipboard:append('unnamedplus')
+vim.opt.hidden = true
+vim.opt.ignorecase = true
+vim.opt.linebreak = true
+vim.opt.list = true
 vim.opt.mouse = 'a'
+vim.opt.number = true
+vim.opt.path:append('**')
+vim.opt.scrolloff = 3
+vim.opt.showmatch = true
+vim.opt.smartcase = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.termguicolors = true
 
 -- Indentation
 vim.opt.expandtab = false
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
 vim.cmd([[
 	"" Set local indentation preferences *after* ftplugins
 	"" but *before* plugin scripts (eg, sleuth, editorconfig)
