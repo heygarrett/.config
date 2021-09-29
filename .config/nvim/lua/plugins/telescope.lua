f48e827d3ecbe3ffa63f076eaa51b541bfa81dcd
Garrett Oreilly <hey@heygarrett.com>
2021-09-29 13:18:59 -0400

    Added telescope commands for LSP lists

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -14,6 +14,12 @@ vim.cmd([[
 	command! Grep Telescope live_grep
 	command! Bufs Telescope buffers
 	command! Help Telescope help_tags
+	" LSP lists
+	command! Refs Telescope lsp_references
+	command! Defs Telescope lsp_definitions
+	command! Imps Telescope lsp_implementations
+	command! Acts Telescope lsp_code_actions
+	command! Doc Telescope lsp_document_diagnostics
 ]])
 
 local telescope = require('telescope')
