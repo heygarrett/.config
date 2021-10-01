c7146ac222b307279cbf58d0fcc5b0cbe9a2cac9
Garrett Oreilly <hey@heygarrett.com>
2021-10-01 02:11:50 -0400

    Tidying

--- .config/nvim/lua/plugins/packer.lua
+++ .config/nvim/lua/plugins/packer.lua
@@ -1,6 +1,6 @@
 vim.api.nvim_command('autocmd BufWritePost packer.lua source <afile> | PackerCompile')
-local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 
+local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
 	vim.fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
 	vim.api.nvim_command('packadd packer.nvim')
