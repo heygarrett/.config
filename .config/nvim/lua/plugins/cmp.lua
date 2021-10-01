b14c301d32b2d0f898b721d41c06a82f0b81e215
Garrett Oreilly <hey@heygarrett.com>
2021-10-01 18:48:05 -0400

    Don't need these mappings

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -9,10 +9,6 @@ cmp.setup({
 		end
 	},
 	mapping = {
-		['<C-d>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-e>'] = cmp.mapping.close(),
 		['<CR>'] = cmp.mapping.confirm({
 			behavior = cmp.ConfirmBehavior.Replace,
 			select = true
