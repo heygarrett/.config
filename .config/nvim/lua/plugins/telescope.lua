36feea90ea46300e1c32cb05be5799750530c41c
Garrett Oreilly <hey@heygarrett.com>
2021-10-01 01:29:47 -0400

    Playing with LSP keymaps

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,11 +1,7 @@
 local map = vim.api.nvim_set_keymap
 local opts = {noremap = true, silent = true}
 
-map('n', '<leader>ff', '<cmd>Telescope find_files<cr>', opts)
-map('n', '<leader>ft', '<cmd>Telescope git_files<cr>', opts)
-map('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', opts)
-map('n', '<leader>fb', '<cmd>Telescope buffers<cr>', opts)
-map('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', opts)
+map('n', '<c-]>', '<cmd>Telescope lsp_definitions<cr>', opts)
 
 vim.cmd([[
 	command! Browse Telescope file_browser
--- .config/nvim/lua/utils/on-attach.lua
+++ .config/nvim/lua/utils/on-attach.lua
@@ -6,7 +6,7 @@ local on_attach = function(_, bufnr)
 
 	-- See `:help vim.lsp.*` for documentation on any of the below functions
 	buf_set_keymap('n', '<leader>dec', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-	buf_set_keymap('n', '<c-]>', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+	buf_set_keymap('n', '<leader>def', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
 	buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
 	buf_set_keymap('n', '<leader>imp', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
 	buf_set_keymap('n', '<leader>sig', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
