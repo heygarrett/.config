bb8d8a684b6b8c480e7b0bd4cbc20ab8329c49df
Garrett Oreilly <hey@heygarrett.com>
2021-10-08 19:15:47 -0400

    Color line numbers for diagnostics

--- .config/nvim/lua/utils/diagnostic-signs.lua
+++ .config/nvim/lua/utils/diagnostic-signs.lua
@@ -1,7 +1,6 @@
 vim.opt.signcolumn = 'yes'
 
-local signs = { Error = '🚫', Warn = '⚠️', Hint = '💡', Info = 'ℹ️' }
-for type, icon in pairs(signs) do
+for _, type in ipairs({'Error', 'Warn', 'Hint', 'Info'}) do
     local hl = 'DiagnosticSign' .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
+    vim.fn.sign_define(hl, { text = '', texthl = hl, numhl = hl})
 end
