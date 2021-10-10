064c11e07280550e6d225a078d5745791dda9d0b
Garrett Oreilly <hey@heygarrett.com>
2021-10-10 00:15:39 -0400

    Tidying

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -19,7 +19,6 @@ vim.opt.expandtab = false
 vim.opt.shiftwidth = 0
 vim.opt.softtabstop = -1
 vim.opt.tabstop = 4
-
 -- Set indentation *after* ftplugins but *before* plugin scripts (eg, sleuth, editorconfig)
 vim.cmd [[
 	filetype plugin indent on
