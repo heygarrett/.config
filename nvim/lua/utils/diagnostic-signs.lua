4482465fb39e1f0a45ca5ee553192ff173afc1ce
Garrett Oreilly <hey@heygarrett.com>
2021-10-24 19:51:10 -0400

    Tidying

--- nvim/lua/utils/diagnostic-signs.lua
+++ nvim/lua/utils/diagnostic-signs.lua
@@ -1,4 +1,4 @@
 for _, type in ipairs({ 'Error', 'Warn', 'Hint', 'Info' }) do
-    local hl = 'DiagnosticSign' .. type
-    vim.fn.sign_define(hl, { text = '', texthl = hl, numhl = hl })
+	local hl = 'DiagnosticSign' .. type
+	vim.fn.sign_define(hl, { text = '', texthl = hl, numhl = hl })
 end
