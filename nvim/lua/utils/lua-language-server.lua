434038825bfab905f3e402d773c0038abe05c60c
Garrett Oreilly <hey@heygarrett.com>
2021-10-25 01:08:08 -0400

    Consistency

--- nvim/lua/utils/lua-language-server.lua
+++ nvim/lua/utils/lua-language-server.lua
@@ -1,20 +1,20 @@
 local system_name
-if vim.fn.has("mac") == 1 then
-	system_name = "macOS"
-elseif vim.fn.has("unix") == 1 then
-	system_name = "Linux"
+if vim.fn.has('mac') == 1 then
+	system_name = 'macOS'
+elseif vim.fn.has('unix') == 1 then
+	system_name = 'Linux'
 elseif vim.fn.has('win32') == 1 then
-	system_name = "Windows"
+	system_name = 'Windows'
 else
-	print("Unsupported system for sumneko")
+	print('Unsupported system for sumneko')
 end
 
 local sumneko_root_path = vim.env.HOME .. '/.local/src/lua-language-server'
 local sumneko_binary = sumneko_root_path .. '/bin/' .. system_name .. '/lua-language-server'
 
 local runtime_path = vim.split(package.path, ';')
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
+table.insert(runtime_path, 'lua/?.lua')
+table.insert(runtime_path, 'lua/?/init.lua')
 
 -- TODO: figure out how to not duplicate these from lspconfig
 local on_attach = require('utils/on-attach')
@@ -23,7 +23,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 require('lspconfig').sumneko_lua.setup {
 	on_attach = on_attach,
 	capabilities = capabilities,
-	cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
+	cmd = { sumneko_binary, '-E', sumneko_root_path .. '/main.lua' },
 	settings = {
 		Lua = {
 			runtime = {
@@ -34,7 +34,7 @@ require('lspconfig').sumneko_lua.setup {
 				globals = { 'vim' },
 			},
 			workspace = {
-				library = vim.api.nvim_get_runtime_file("", true),
+				library = vim.api.nvim_get_runtime_file('', true),
 			},
 			telemetry = {
 				enable = false,
