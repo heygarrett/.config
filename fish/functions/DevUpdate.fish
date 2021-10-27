219efe7ca2816b57ba22af6d2d73294ef40b9f74
Garrett Oreilly <hey@heygarrett.com>
2021-10-26 21:40:53 -0400

    Only recompile Lua LSP if there are updates

--- fish/functions/DevUpdate.fish
+++ fish/functions/DevUpdate.fish
@@ -1,8 +1,11 @@
 function DevUpdate
 	brew update && brew upgrade && brew cleanup
 	and brew upgrade --fetch-HEAD neovim
-	and cd $HOME/.local/src/lua-language-server
-	and git pull && git submodule update --recursive
-	and pushd 3rd/luamake && compile/install.sh && popd && ./3rd/luamake/luamake rebuild
+	and cd $HOME/.local/src/lua-language-server && git fetch
+	and set -l updates (git log HEAD..origin/master --oneline)
+	and if updates != ''
+		git merge && git submodule update --recursive
+		and pushd 3rd/luamake && compile/install.sh && popd && ./3rd/luamake/luamake rebuild
+	end
 	and cd $HOME/.config/nvim && nvim lua/plugins/packer.lua +PackerSync
 end
