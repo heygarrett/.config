0db5750edca5eced19b85c8c15711a13413b7482
Garrett Oreilly <hey@heygarrett.com>
2021-10-27 11:52:52 -0400

    Fixed Lua LSP update mechanism
    Fish demands double quotes

--- fish/functions/DevUpdate.fish
+++ fish/functions/DevUpdate.fish
@@ -3,7 +3,7 @@ function DevUpdate
 	and brew upgrade --fetch-HEAD neovim
 	and cd $HOME/.local/src/lua-language-server && git fetch
 	and set -l updates (git log HEAD..origin/master --oneline)
-	and if updates != ''
+	and if test -n "$updates"
 		git merge && git submodule update --recursive
 		and pushd 3rd/luamake && compile/install.sh && popd && ./3rd/luamake/luamake rebuild
 	end
