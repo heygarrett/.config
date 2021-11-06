6761d423dfb7210848569b6242073bd852c45f84
Garrett Oreilly <hey@heygarrett.com>
2021-11-06 11:36:48 -0400

    Tidying

--- nvim/lua/utils/on-attach.lua
+++ nvim/lua/utils/on-attach.lua
@@ -9,21 +9,21 @@ local on_attach = function(_, bufnr)
 	local opts = { noremap=true, silent=true }
 
 	-- Keymaps
+	buf_set_keymap('n', '<leader>d', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+	buf_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 	buf_set_keymap('n', '<leader>h', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
 	buf_set_keymap('n', '<leader>s', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
 	buf_set_keymap('n', '<leader>t', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-	buf_set_keymap('n', '<leader>d', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
 	buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
 	buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-	buf_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 
 	-- Commands
 	-- vim.cmd [[
-	-- 	command! -buffer Defs lua vim.lsp.buf.definition()
-	-- 	command! -buffer Imps lua vim.lsp.buf.implementation()
 	-- 	command! -buffer Acts lua vim.lsp.buf.code_action()
-	-- 	command! -buffer Refs lua vim.lsp.buf.references()
+	-- 	command! -buffer Defs lua vim.lsp.buf.definition()
 	-- 	command! -buffer Doc lua vim.lsp.diagnostic.set_loclist()
+	-- 	command! -buffer Imps lua vim.lsp.buf.implementation()
+	-- 	command! -buffer Refs lua vim.lsp.buf.references()
 	-- ]]
 
 end
