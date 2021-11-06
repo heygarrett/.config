5e1a4e6870aaf574ce8fc1bf265bac2101456789
Garrett Oreilly <hey@heygarrett.com>
2021-11-06 11:29:38 -0400

    Added option to manually rebuild Lua LSP

--- fish/functions/DevUpdate.fish
+++ fish/functions/DevUpdate.fish
@@ -2,7 +2,7 @@ function DevUpdate
 	brew update && brew upgrade --fetch-HEAD
 	and cd $HOME/.local/src/lua-language-server && git fetch
 	and set -l updates (git log HEAD..origin/master --oneline)
-	and if test -n "$updates"
+	and if contains -- -b "$argv" -o test -n "$updates"
 		git merge && git submodule update --recursive
 		and pushd 3rd/luamake && compile/install.sh && popd && ./3rd/luamake/luamake rebuild
 	end
