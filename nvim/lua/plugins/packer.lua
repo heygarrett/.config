80f26df8ec5fbd3c9d6d1f43848c62cf30e0b36b
Garrett Oreilly <hey@heygarrett.com>
2021-11-08 02:41:33 -0500

    Updating packer config to reflect changes to docs

--- nvim/lua/plugins/packer.lua
+++ nvim/lua/plugins/packer.lua
@@ -1,32 +1,41 @@
-vim.api.nvim_command('autocmd BufWritePost packer.lua source <afile> | PackerCompile')
-
 local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-	vim.fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+	Bootstrap = vim.fn.system {
+		'git', 'clone', '--depth', '1',
+		'https://github.com/wbthomason/packer.nvim',
+		install_path
+	}
 end
 
 require('packer').startup {
-	{
-		-- Self-manage
-		'wbthomason/packer.nvim',
+	function(use)
+		use {
+			-- Self-manage
+			'wbthomason/packer.nvim',
 
-		-- No configs
-		'editorconfig/editorconfig-vim',
-		'farmergreg/vim-lastplace',
-		'tpope/vim-fugitive',
-		'tpope/vim-sleuth',
-		'tpope/vim-vinegar',
+			-- No configs
+			'editorconfig/editorconfig-vim',
+			'farmergreg/vim-lastplace',
+			'tpope/vim-fugitive',
+			'tpope/vim-sleuth',
+			'tpope/vim-vinegar',
 
-		-- Configs
-		require('plugins/Comment'),
-		require('plugins/lualine'),
-		require('plugins/nvim-lspconfig'),
-		require('plugins/nvim-treesitter'),
-		require('plugins/telescope'),
-		require('plugins/tokyonight'),
-		require('plugins/vim-indexed-search'),
-		require('plugins/vim-markdown'),
-	},
+			-- Configs
+			require('plugins/Comment'),
+			require('plugins/lualine'),
+			require('plugins/nvim-lspconfig'),
+			require('plugins/nvim-treesitter'),
+			require('plugins/telescope'),
+			require('plugins/tokyonight'),
+			require('plugins/vim-indexed-search'),
+			require('plugins/vim-markdown'),
+		}
+
+		if Bootstrap then
+			require('packer').sync()
+		end
+	end,
+
 	config = {
 		display = {
 			open_fn = require('packer.util').float,
