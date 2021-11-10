a37a55bf33f4436943a73192e85285a3b5886e66
Garrett Oreilly <hey@heygarrett.com>
2021-11-10 17:57:26 -0500

    Trying out kitty

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 starship init fish | source
 set -gx GPG_TTY (tty)
 
-if status is-interactive
-and not set -q TMUX
-	tmux attach || tmux new-session -s G
-end
+# if status is-interactive
+# and not set -q TMUX
+# 	tmux attach || tmux new-session -s G
+# end
