aabbe2c33b14b23366db5d20e6ba8bdaadce8ca7
Garrett Oreilly <hey@heygarrett.com>
2021-11-14 15:07:44 -0500

    Updated indentation autocmd for new organization

--- nvim/plugin/indentation.lua
+++ nvim/plugin/indentation.lua
@@ -3,7 +3,4 @@ vim.opt.shiftwidth = 0
 vim.opt.softtabstop = -1
 vim.opt.tabstop = 4
 -- Set indentation *after* ftplugins but *before* plugin scripts (eg, sleuth, editorconfig)
-vim.cmd [[
-	filetype plugin indent on
-	autocmd FileType * set expandtab< shiftwidth< softtabstop< tabstop<
-]]
+vim.api.nvim_command([[autocmd FileType * set expandtab< shiftwidth< softtabstop< tabstop<]])
