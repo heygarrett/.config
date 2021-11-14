55fe4f67b48356763f7c9fab19844f9473189e9c
Garrett Oreilly <hey@heygarrett.com>
2021-11-14 15:13:35 -0500

    Forgot to change autocmd to au!

--- nvim/plugin/indentation.lua
+++ nvim/plugin/indentation.lua
@@ -3,4 +3,4 @@ vim.opt.shiftwidth = 0
 vim.opt.softtabstop = -1
 vim.opt.tabstop = 4
 -- Set indentation *after* ftplugins but *before* plugin scripts (eg, sleuth, editorconfig)
-vim.api.nvim_command([[autocmd FileType * set expandtab< shiftwidth< softtabstop< tabstop<]])
+vim.api.nvim_command([[au! FileType * set expandtab< shiftwidth< softtabstop< tabstop<]])
