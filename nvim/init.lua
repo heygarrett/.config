ffe22754772b470f1389e54740651e80fc18bb9d
Garrett Oreilly <hey@heygarrett.com>
2021-11-13 23:32:50 -0500

    TIL about :Ex

--- nvim/init.lua
+++ nvim/init.lua
@@ -13,7 +13,6 @@ vim.cmd [[
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 map('i', '<c-space>', '<c-x><c-o>', opts)
-map('n', '<leader>-', '<cmd>e %:h<cr>', opts)
 map('n', '<s-tab>', '<c-o>', opts)
 map('n', 'j', 'gj', opts)
 map('n', 'k', 'gk', opts)
