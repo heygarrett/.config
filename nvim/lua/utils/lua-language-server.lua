918b7a3dacfbf002c185833ce7a942725fda9fa8
Garrett Oreilly <hey@heygarrett.com>
2021-12-04 12:27:29 -0500

    Simplified Lua LSP config

--- nvim/lua/utils/lua-language-server.lua
+++ nvim/lua/utils/lua-language-server.lua
@@ -1,17 +1,3 @@
-local system_name
-if vim.fn.has('mac') == 1 then
-	system_name = 'macOS'
-elseif vim.fn.has('unix') == 1 then
-	system_name = 'Linux'
-elseif vim.fn.has('win32') == 1 then
-	system_name = 'Windows'
-else
-	print('Unsupported system for sumneko')
-end
-
-local sumneko_root_path = vim.env.HOME .. '/.local/src/lua-language-server'
-local sumneko_binary = sumneko_root_path .. '/bin/' .. system_name .. '/lua-language-server'
-
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, 'lua/?.lua')
 table.insert(runtime_path, 'lua/?/init.lua')
@@ -19,7 +5,7 @@ table.insert(runtime_path, 'lua/?/init.lua')
 require('lspconfig').sumneko_lua.setup {
 	on_attach = require('utils/on-attach'),
 	capabilities = vim.lsp.protocol.make_client_capabilities(),
-	cmd = { sumneko_binary, '-E', sumneko_root_path .. '/main.lua' },
+	cmd = { vim.env.HOME .. '/.local/src/lua-language-server/bin/macOS/lua-language-server' },
 	settings = {
 		Lua = {
 			runtime = {
