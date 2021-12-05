80c7ea0f1707436320d2391f06cee5e8b6589488
Garrett Oreilly <hey@heygarrett.com>
2021-12-04 23:54:59 -0500

    Replace type def check with definition check

--- nvim/lua/utils/on-attach.lua
+++ nvim/lua/utils/on-attach.lua
@@ -14,7 +14,7 @@ local on_attach = function(_, bufnr)
 	buf_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<cr>', opts)
 	buf_set_keymap('n', '<leader>h', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
 	buf_set_keymap('n', '<leader>r', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-	buf_set_keymap('n', '<leader>t', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+	buf_set_keymap('n', '<leader>s', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
 	buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>', opts)
 	buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<cr>', opts)
 end
