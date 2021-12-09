bbd2f9a1c8ee0fa4b1309ab548dac8db70596c6f
Garrett Oreilly <hey@heygarrett.com>
2021-12-09 13:47:57 -0500

    Updated LSP diagnostics keymaps

--- nvim/lua/utils/on-attach.lua
+++ nvim/lua/utils/on-attach.lua
@@ -10,13 +10,13 @@ local on_attach = function(_, bufnr)
 
 	-- Keymaps
 	buf_set_keymap('i', '<c-s>', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-	buf_set_keymap('n', '<leader>d', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<cr>', opts)
+	buf_set_keymap('n', '<leader>d', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
 	buf_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<cr>', opts)
 	buf_set_keymap('n', '<leader>h', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
 	buf_set_keymap('n', '<leader>r', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
 	buf_set_keymap('n', '<leader>s', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-	buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>', opts)
-	buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<cr>', opts)
+	buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
+	buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts)
 end
 
 return on_attach
