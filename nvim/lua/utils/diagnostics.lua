b2ff1a1b032d4f994a61485413ef09cbbfab0275
Garrett Oreilly <hey@heygarrett.com>
2021-12-11 21:59:52 -0500

    Disabling virtual text

--- nvim/lua/utils/diagnostics.lua
+++ nvim/lua/utils/diagnostics.lua
@@ -2,3 +2,7 @@ for _, type in ipairs({ 'Error', 'Warn', 'Hint', 'Info' }) do
 	local hl = 'DiagnosticSign' .. type
 	vim.fn.sign_define(hl, { text = '', texthl = hl, numhl = hl })
 end
+
+vim.diagnostic.config {
+	virtual_text = false
+}
