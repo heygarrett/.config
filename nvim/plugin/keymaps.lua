b44f25ee3e83e41f703ad3dfc81075b0dc379d56
Garrett Oreilly <hey@heygarrett.com>
2021-12-13 23:00:47 -0500

    Not using this keymap

--- nvim/plugin/keymaps.lua
+++ nvim/plugin/keymaps.lua
@@ -1,7 +1,6 @@
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 map('i', '<c-space>', '<c-x><c-o>', opts)
-map('i', '<leader>{<cr>', '{<cr>}<esc>O', opts)
 map('n', '<s-tab>', '<c-o>', opts)
 map('n', 'j', 'gj', opts)
 map('n', 'k', 'gk', opts)
