3a730b8e2196d4c9ae8dc30f15e7375405c4f983
Garrett Oreilly <hey@heygarrett.com>
2022-04-23 16:38:15 -0400

    Setting nil seems cleaner than setting global

--- nvim/plugin/indentation.lua
+++ nvim/plugin/indentation.lua
@@ -5,9 +5,9 @@ vim.opt.tabstop = 4
 -- Set indentation *after* ftplugins
 vim.api.nvim_create_autocmd('FileType', {
 	callback = function()
-		vim.opt_local.expandtab = vim.opt_global.expandtab:get()
-		vim.opt_local.shiftwidth = vim.opt_global.shiftwidth:get()
-		vim.opt_local.softtabstop = vim.opt_global.softtabstop:get()
-		vim.opt_local.tabstop = vim.opt_global.tabstop:get()
+		vim.opt_local.expandtab = nil
+		vim.opt_local.shiftwidth = nil
+		vim.opt_local.softtabstop = nil
+		vim.opt_local.tabstop = nil
 	end
 })
