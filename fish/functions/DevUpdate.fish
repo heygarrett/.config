df274c2ef8d276310989a2bb9bf39f3c171fa948
Garrett Oreilly <hey@heygarrett.com>
2022-05-08 15:08:15 -0400

    Added PackerSnapshot before each PackerSync

--- fish/functions/DevUpdate.fish
+++ fish/functions/DevUpdate.fish
@@ -1,5 +1,7 @@
 function DevUpdate
 	brew update && brew upgrade --fetch-HEAD
 	and npm -g update
-	and cd $HOME/.config/nvim && nvim lua/config/packer.lua +PackerSync
+	and cd $HOME/.config/nvim
+	and nvim lua/config/packer.lua \
+	"+PackerSnapshotDelete latest" "+PackerSnapshot latest" "+PackerSync"
 end
