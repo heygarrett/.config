5bb24bf73987fe48f34272e8e706365ea594297f
Garrett Oreilly <hey@heygarrett.com>
2022-05-27 13:21:47 -0400

    Switched from ' to "

--- nvim/plugin/statusline.lua
+++ nvim/plugin/statusline.lua
@@ -1,10 +1,10 @@
 local function file_name()
 	local root_path = vim.fn.getcwd()
-	local root_dir = root_path:match('[^/]+$')
-	local home_path = vim.fn.expand('%:~')
+	local root_dir = root_path:match("[^/]+$")
+	local home_path = vim.fn.expand("%:~")
 	local overlap, _ = home_path:find(root_dir)
-	if home_path == '' then
-		return root_path:gsub('/Users/garrett', '~')
+	if home_path == "" then
+		return root_path:gsub("/Users/garrett", "~")
 	elseif overlap then
 		return home_path:sub(overlap)
 	else
@@ -13,28 +13,29 @@ local function file_name()
 end
 
 local function progress()
-	if vim.fn.line('.') == 1 then
-		return 'top'
-	elseif vim.fn.line('.') == vim.fn.line('$') then
-		return 'bot'
+	if vim.fn.line(".") == 1 then
+		return "top"
+	elseif vim.fn.line(".") == vim.fn.line("$") then
+		return "bot"
 	else
-		local p = vim.fn.line('.') / vim.fn.line('$') * 100
+		local p = vim.fn.line(".") / vim.fn.line("$") * 100
 		p = p % 1 >= .5 and math.ceil(p) or math.floor(p)
-		return string.format('%02d', p) .. '%%'
+		return string.format("%02d", p) .. "%%"
 	end
 end
 
 function _G.status_line()
-	return '%<'
+	return " "
+		.. "%<"
 		.. file_name()
-		.. ' '
-		.. '%h'
-		.. '%m'
-		.. '%='
-		.. '%y'
-		.. ' '
+		.. " "
+		.. "%h"
+		.. "%m"
+		.. "%="
+		.. "%y"
+		.. " "
 		.. progress()
-		.. ' '
+		.. " "
 end
 
-vim.opt.statusline = '%{%v:lua.status_line()%}'
+vim.opt.statusline = "%{%v:lua.status_line()%}"
