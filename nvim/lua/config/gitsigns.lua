f89a75b99226a1c0e917eb3d739931caf305c395
Garrett Oreilly <hey@heygarrett.com>
2022-07-02 23:29:12 -0400

    Add Reset command for gitsigns

--- nvim/lua/config/gitsigns.lua
+++ nvim/lua/config/gitsigns.lua
@@ -24,17 +24,18 @@ return {
 					return "<Ignore>"
 				end, { expr = true })
 
-				local function stage(t)
+				local function hunk_range(f, t)
 					if t.range ~= 0 then
-						gs.stage_hunk({ t.line1, t.line2 })
+						f({ t.line1, t.line2 })
 					else
-						gs.stage_hunk()
+						f()
 					end
 				end
 
 				vim.api.nvim_create_user_command("Blame", function() gs.blame_line({ full = true }) end, {})
 				vim.api.nvim_create_user_command("Diff", gs.preview_hunk, {})
-				vim.api.nvim_create_user_command("Stage", function(t) stage(t) end, { range = true })
+				vim.api.nvim_create_user_command("Reset", function(t) hunk_range(gs.reset_hunk, t) end, { range = true })
+				vim.api.nvim_create_user_command("Stage", function(t) hunk_range(gs.stage_hunk, t) end, { range = true })
 				vim.api.nvim_create_user_command("Unstage", gs.undo_stage_hunk, {})
 			end
 		}
