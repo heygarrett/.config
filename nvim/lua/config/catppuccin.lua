42e040f2f417405a3460e19bf4d26bcb40e6e881
Garrett Oreilly <hey@heygarrett.com>
2022-07-06 14:20:11 -0400

    "" instead of [[]] for vim commands where possible

--- nvim/lua/config/catppuccin.lua
+++ nvim/lua/config/catppuccin.lua
@@ -2,6 +2,6 @@ return {
 	"catppuccin/nvim",
 	config = function()
 		vim.g.catppuccin_flavour = "mocha"
-		vim.api.nvim_command([[colorscheme catppuccin]])
+		vim.api.nvim_command("colorscheme catppuccin")
 	end
 }
