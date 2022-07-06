9f5f3d8da3fac7e079f0d82b548aa6ec3f514940
Garrett Oreilly <hey@heygarrett.com>
2022-07-05 20:31:49 -0400

    Don't guess indent in netrw
    Need to come back to this

--- nvim/lua/settings/indentation.lua
+++ nvim/lua/settings/indentation.lua
@@ -13,7 +13,9 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.opt_local.softtabstop = vim.opt_global.softtabstop:get()
 		vim.opt_local.tabstop = vim.opt_global.tabstop:get()
 		-- Run guess-indent
-		if vim.opt.filetype:get() ~= "gitcommit" then
+		if vim.opt.filetype:get() ~= "gitcommit" and
+			vim.opt.filetype:get() ~= "netrw"
+		then
 			vim.api.nvim_command("GuessIndent")
 		end
 		-- Use | for tabs and · for spaces
