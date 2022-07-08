49884bb1c1619211be21a164f098c32356221ace
Garrett Oreilly <hey@heygarrett.com>
2022-07-07 20:03:35 -0400

    Wrap remaining autocmds in groups

--- nvim/lua/settings/terminal.lua
+++ nvim/lua/settings/terminal.lua
@@ -1,6 +1,8 @@
-vim.api.nvim_create_autocmd("TermOpen", { command = "startinsert" })
+vim.api.nvim_create_augroup("terminal", { clear = true })
 vim.api.nvim_create_autocmd("TermOpen", {
+	group = "terminal",
 	callback = function()
+		vim.api.nvim_command("startinsert")
 		vim.opt.cursorline = false
 		vim.opt.number = false
 	end
