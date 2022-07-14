60d9064d8823786c720648d2831863efbc596120
Garrett Oreilly <hey@heygarrett.com>
2022-07-13 21:51:05 -0400

    Formatting

--- nvim/lua/config/gitsigns.lua
+++ nvim/lua/config/gitsigns.lua
@@ -13,7 +13,7 @@ return {
 				end
 
 				map("n", "]h", function()
-				if vim.wo.diff then return "]h" end
+					if vim.wo.diff then return "]h" end
 					vim.schedule(function() gs.next_hunk() end)
 					return "<Ignore>"
 				end, { expr = true })
@@ -32,11 +32,17 @@ return {
 					end
 				end
 
-				vim.api.nvim_create_user_command("Blame", function() gs.blame_line({ full = true }) end, {})
 				vim.api.nvim_create_user_command("Diff", gs.preview_hunk, {})
-				vim.api.nvim_create_user_command("Reset", function(t) hunk_range(gs.reset_hunk, t) end, { range = true })
-				vim.api.nvim_create_user_command("Stage", function(t) hunk_range(gs.stage_hunk, t) end, { range = true })
 				vim.api.nvim_create_user_command("Unstage", gs.undo_stage_hunk, {})
+				vim.api.nvim_create_user_command("Blame", function()
+					gs.blame_line({ full = true })
+				end, {})
+				vim.api.nvim_create_user_command("Reset", function(t)
+					hunk_range(gs.reset_hunk, t)
+				end, { range = true })
+				vim.api.nvim_create_user_command("Stage", function(t)
+					hunk_range(gs.stage_hunk, t)
+				end, { range = true })
 			end
 		}
 	end
