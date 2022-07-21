5b89b2f41a8062c8ad83c00738e35c04df208f88
Garrett Oreilly <hey@heygarrett.com>
2022-07-21 18:39:09 -0400

    Use gitsigns buffer functions for % range

--- nvim/lua/config/gitsigns.lua
+++ nvim/lua/config/gitsigns.lua
@@ -32,11 +32,15 @@ return {
 					return "<Ignore>"
 				end, { expr = true })
 
-				local function hunk_range(f, t)
-					if t.range ~= 0 then
-						f({ t.line1, t.line2 })
+				local function hunk_range(stage, selection)
+					if selection.range ~= 0 then
+						if selection.line1 == 1 and selection.line2 == vim.fn.line("$") then
+							stage["buffer"]()
+						else
+							stage["hunk"]({ selection.line1, selection.line2 })
+						end
 					else
-						f()
+						stage["hunk"]()
 					end
 				end
 
@@ -45,11 +49,11 @@ return {
 				vim.api.nvim_create_user_command("Blame", function()
 					gs.blame_line({ full = true })
 				end, {})
-				vim.api.nvim_create_user_command("Reset", function(t)
-					hunk_range(gs.reset_hunk, t)
+				vim.api.nvim_create_user_command("Reset", function(selection)
+					hunk_range({ hunk = gs.reset_hunk, buffer = gs.reset_buffer }, selection)
 				end, { range = true })
-				vim.api.nvim_create_user_command("Stage", function(t)
-					hunk_range(gs.stage_hunk, t)
+				vim.api.nvim_create_user_command("Stage", function(selection)
+					hunk_range({ hunk = gs.stage_hunk, buffer = gs.stage_buffer }, selection)
 				end, { range = true })
 			end,
 		})
