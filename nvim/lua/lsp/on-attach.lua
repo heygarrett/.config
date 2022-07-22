5421f5a12bd3b3dc08706c4012226c10d0cf97e6
Garrett Oreilly <hey@heygarrett.com>
2022-07-21 21:39:51 -0400

    Sort diagnostic keymaps

--- nvim/lua/lsp/on-attach.lua
+++ nvim/lua/lsp/on-attach.lua
@@ -14,9 +14,9 @@ local on_attach = function(client, bufnr)
 
 	-- Diagnostics
 	vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float)
+	vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
 	vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
 	vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-	vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
 
 	-- LSP settings
 	local opts = { buffer = bufnr }
