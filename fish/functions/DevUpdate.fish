c5c0d49bd2bd0a0192e8083402097bb17b4db7ec
Garrett Oreilly <hey@heygarrett.com>
2022-07-21 23:55:57 -0400

    Change dirs for DevUpdate command

--- fish/functions/DevUpdate.fish
+++ fish/functions/DevUpdate.fish
@@ -2,7 +2,7 @@ function DevUpdate
 	brew update && brew upgrade --fetch-HEAD
 	and npm -g update
 	and fisher update
-	and cd $HOME/.config/nvim
-	and nvim plugin/packer.lua \
+	and cd $HOME/.config
+	and nvim nvim/plugin/packer.lua \
 		"+PackerSnapshotDelete latest" "+PackerSnapshot latest" "+PackerSync"
 end
