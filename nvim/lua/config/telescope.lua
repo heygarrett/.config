e30f8e026f0e579864104976078796f92fae4b3b
Garrett Oreilly <hey@heygarrett.com>
2022-07-21 22:29:01 -0400

    Replace Telescope vim commands with Lua functions

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -8,19 +8,30 @@ return {
 		},
 	},
 	config = function()
-		vim.api.nvim_create_user_command("Bufs", "Telescope buffers", {})
-		vim.api.nvim_create_user_command("Find", "Telescope find_files", {})
-		vim.api.nvim_create_user_command("Tracked", "Telescope git_files", {})
-		vim.api.nvim_create_user_command("Grep", "Telescope live_grep", {})
-		vim.api.nvim_create_user_command("Help", "Telescope help_tags", {})
+		local telescope = require("telescope.builtin")
+		vim.api.nvim_create_user_command("Bufs", telescope.buffers, {})
+		vim.api.nvim_create_user_command("Grep", telescope.live_grep, {})
+		vim.api.nvim_create_user_command("Help", telescope.help_tags, {})
+		vim.api.nvim_create_user_command("Find", function()
+			if vim.fn.system("git rev-parse --is-inside-work-tree"):match("true") then
+				telescope.git_files({ show_untracked = true })
+			else
+				telescope.find_files()
+			end
+		end, {})
 		-- LSP lists
-		vim.api.nvim_create_user_command("Acts", "Telescope lsp_code_actions", {})
-		vim.api.nvim_create_user_command("Defs", "Telescope lsp_definitions", {})
-		vim.api.nvim_create_user_command("Diags", "Telescope diagnostics bufnr=0", {})
-		vim.api.nvim_create_user_command("Imps", "Telescope lsp_implementations", {})
-		vim.api.nvim_create_user_command("Refs", "Telescope lsp_references", {})
+		vim.api.nvim_create_user_command("Defs", telescope.lsp_definitions, {})
+		vim.api.nvim_create_user_command("Imps", telescope.lsp_implementations, {})
+		vim.api.nvim_create_user_command("Refs", telescope.lsp_references, {})
+		vim.api.nvim_create_user_command("Diags", function(t)
+			if t.args == "all" then
+				telescope.diagnostics({ bufnr = nil })
+			else
+				telescope.diagnostics({ bufnr = 0 })
+			end
+		end, { nargs = "?" })
 
-		require("telescope").setup({})
+		require("telescope").setup()
 		require("telescope").load_extension("fzf")
 	end,
 }
