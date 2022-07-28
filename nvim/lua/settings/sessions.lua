2b38d8bbf9981ef9d6d5d8a54a77ea5bdc674b2f
Garrett Oreilly <hey@heygarrett.com>
2022-07-28 15:05:59 -0400

    Auto-save session when quitting Neovim

--- nvim/lua/settings/sessions.lua
+++ nvim/lua/settings/sessions.lua
@@ -1,2 +1,7 @@
 vim.api.nvim_create_user_command("Save", "mksession!", {})
 vim.api.nvim_create_user_command("Load", "silent! source Session.vim", {})
+
+vim.api.nvim_create_autocmd("VimLeavePre", {
+	group = vim.api.nvim_create_augroup("sessions", { clear = true }),
+	command = "Save",
+})
