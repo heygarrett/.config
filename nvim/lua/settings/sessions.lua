c36b47f8884f08b39a89baed35d93844786a4e31
Garrett Oreilly <hey@heygarrett.com>
2022-07-28 16:07:04 -0400

    Let autocmds handle sessions; remove user commands

--- nvim/lua/settings/sessions.lua
+++ nvim/lua/settings/sessions.lua
@@ -1,17 +1,14 @@
-vim.api.nvim_create_user_command("Save", "mksession!", {})
-vim.api.nvim_create_user_command("Load", "silent! source Session.vim", {})
-
 vim.api.nvim_create_augroup("sessions", { clear = true })
 vim.api.nvim_create_autocmd("VimEnter", {
 	group = "sessions",
 	nested = true,
 	callback = function()
 		if vim.fn.argc() == 0 then
-			vim.api.nvim_command("Load")
+			vim.api.nvim_command("silent! source Session.vim")
 		end
 	end,
 })
 vim.api.nvim_create_autocmd("VimLeavePre", {
 	group = "sessions",
-	command = "Save",
+	command = "mksession!",
 })
