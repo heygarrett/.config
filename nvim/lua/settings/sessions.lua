e13ac12cd94f53ab8db6fbb86ddebcf0ee935e00
Garrett Oreilly <hey@heygarrett.com>
2022-07-28 17:33:56 -0400

    Return early

--- nvim/lua/settings/sessions.lua
+++ nvim/lua/settings/sessions.lua
@@ -4,12 +4,11 @@ vim.api.nvim_create_autocmd("VimEnter", {
 	group = sessions,
 	nested = true,
 	callback = function()
-		if vim.fn.argc() == 0 then
-			if vim.fn.filereadable("Session.vim") == 1 then
-				vim.api.nvim_command("source Session.vim")
-			else
-				vim.api.nvim_command("mksession")
-			end
+		if vim.fn.argc() ~= 0 then return end
+		if vim.fn.filereadable("Session.vim") == 1 then
+			vim.api.nvim_command("source Session.vim")
+		else
+			vim.api.nvim_command("mksession")
 		end
 	end,
 })
