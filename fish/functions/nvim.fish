0aff9a243020eb66b265f990c1d7f148f7eadae0
Garrett Oreilly <hey@heygarrett.com>
2022-07-28 15:55:21 -0400

    Use an autocmd to auto-load Neovim sessions

--- fish/functions/nvim.fish
+++
@@ -1,7 +0,0 @@
-function nvim
-	if not set -q argv[1]
-		command nvim "+Load"
-	else
-		command nvim $argv
-	end
-end
--- nvim/lua/settings/sessions.lua
+++ nvim/lua/settings/sessions.lua
@@ -1,7 +1,17 @@
 vim.api.nvim_create_user_command("Save", "mksession!", {})
 vim.api.nvim_create_user_command("Load", "silent! source Session.vim", {})
 
+vim.api.nvim_create_augroup("sessions", { clear = true })
+vim.api.nvim_create_autocmd("VimEnter", {
+	group = "sessions",
+	nested = true,
+	callback = function()
+		if vim.fn.argc() == 0 then
+			vim.api.nvim_command("Load")
+		end
+	end,
+})
 vim.api.nvim_create_autocmd("VimLeavePre", {
-	group = vim.api.nvim_create_augroup("sessions", { clear = true }),
+	group = "sessions",
 	command = "Save",
 })
