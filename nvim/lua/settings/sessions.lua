7abea60eeb414f147db180ec4a15aca3b78c1ff7
Garrett Oreilly <hey@heygarrett.com>
2022-07-28 22:31:45 -0400

    Handle session guard conditions in function

--- nvim/lua/settings/sessions.lua
+++ nvim/lua/settings/sessions.lua
@@ -1,18 +1,17 @@
--- Determine if nvim launched by git or other process
-local function user_process()
+-- Guard against file args and nvim launched by git
+local function args_or_git()
 	local parent_command =
 		vim.fn.system("ps -o ppid= -p $fish_pid | xargs ps -o comm= -p")
-	return parent_command:match("^nvim")
+	return vim.fn.argc() ~= 0 or not parent_command:match("^nvim")
 end
 
 local sessions = vim.api.nvim_create_augroup("sessions", { clear = true })
--- If launched without args load or create session
+-- Load or create session
 vim.api.nvim_create_autocmd("VimEnter", {
 	group = sessions,
 	nested = true,
 	callback = function()
-		if not user_process() then return end
-		if vim.fn.argc() ~= 0 then return end
+		if args_or_git() then return end
 		if vim.fn.filereadable("Session.vim") == 1 then
 			vim.api.nvim_command("source Session.vim")
 		else
@@ -24,8 +23,7 @@ vim.api.nvim_create_autocmd("VimEnter", {
 vim.api.nvim_create_autocmd("VimLeavePre", {
 	group = sessions,
 	callback = function()
-		if not user_process() then return end
-		if vim.fn.argc() ~= 0 then return end
+		if args_or_git() then return end
 		if vim.fn.filereadable("Session.vim") == 1 then
 			vim.api.nvim_command("mksession!")
 		end
