2d7b2803e58db8cf7764367ef2b67f8c7dcfdb20
Garrett Oreilly <hey@heygarrett.com>
2022-07-30 11:09:44 -0400

    Prompt save session if session exists and has args

--- nvim/lua/settings/sessions.lua
+++ nvim/lua/settings/sessions.lua
@@ -1,17 +1,18 @@
--- Guard against file args and nvim launched by git
-local function args_or_git()
+-- Guard against nvim launched by git
+local function launched_by_git()
 	local parent_command =
 		vim.fn.system("ps -o ppid= -p $fish_pid | xargs ps -o comm= -p")
-	return vim.fn.argc() ~= 0 or not parent_command:match("^nvim")
+	return not parent_command:match("^nvim")
 end
 
 local sessions = vim.api.nvim_create_augroup("sessions", { clear = true })
--- Load or create session
+-- Load or create session on launch
 vim.api.nvim_create_autocmd("VimEnter", {
 	group = sessions,
 	nested = true,
 	callback = function()
-		if args_or_git() then return end
+		if vim.fn.argc() > 0 then return end
+		if launched_by_git() then return end
 		if vim.fn.filereadable("Session.vim") == 1 then
 			vim.api.nvim_command("source Session.vim")
 		else
@@ -23,9 +24,20 @@ vim.api.nvim_create_autocmd("VimEnter", {
 vim.api.nvim_create_autocmd("VimLeavePre", {
 	group = sessions,
 	callback = function()
-		if args_or_git() then return end
-		if vim.fn.filereadable("Session.vim") == 1 then
-			vim.api.nvim_command("mksession!")
+		if launched_by_git() then return end
+		if vim.fn.filereadable("Session.vim") == 0 then return end
+		-- Save session when a session exists and arg list is empty
+		local save = "yes"
+		if vim.fn.argc() > 0 then
+			-- Ask iff session exists but arg list is not empty
+			vim.ui.input(
+				{ prompt = "Save session? [no]: " },
+				function(input) save = input end
+			)
+		end
+		if save and not save:match("^n") then
+			-- Prevent arg list from getting saved in session
+			vim.api.nvim_command("%argd | mksession!")
 		end
 	end,
 })
