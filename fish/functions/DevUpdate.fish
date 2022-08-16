4fe4c730aa5906e77061acb5ca7e5153afc5af9d
Garrett Oreilly <garrett.oreilly@agilebits.com>
2022-08-16 11:37:51 -0400

    Don't fetch changes to .config

--- fish/functions/DevUpdate.fish
+++ fish/functions/DevUpdate.fish
@@ -1,9 +1,8 @@
 function DevUpdate
-	pushd $HOME/.config
-	git fetch && git rebase origin/main
 	brew update && brew upgrade --fetch-HEAD
 	npm --global update
 	fisher update
+	pushd $HOME/.config
 	nvim nvim/plugin/packer.lua \
 		"+PackerSnapshotDelete latest" "+PackerSnapshot latest" "+PackerSync"
 	popd
