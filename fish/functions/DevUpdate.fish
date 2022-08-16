8eea7e4388868635e6a2add6a93706cb8694a0ed
Garrett Oreilly <garrett.oreilly@agilebits.com>
2022-08-16 12:25:25 -0400

    Add dividers to DevUpdate

--- fish/functions/DevUpdate.fish
+++ fish/functions/DevUpdate.fish
@@ -1,7 +1,10 @@
 function DevUpdate
 	brew update && brew upgrade --fetch-HEAD
+	hr
 	npm --global update
+	hr
 	fisher update
+	hr
 	pushd $HOME/.config
 	nvim nvim/plugin/packer.lua \
 		"+PackerSnapshotDelete latest" "+PackerSnapshot latest" "+PackerSync"
