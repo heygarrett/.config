226e1c190b8106104885d44331ba5e3c7278e644
Garrett Oreilly <hey@heygarrett.com>
2022-08-28 23:38:51 -0400

    refactor(completion): move completion config to its own module

--- nvim/lua/lsp/on_attach.lua
+++ nvim/lua/lsp/on_attach.lua
@@ -3,35 +3,7 @@ vim.api.nvim_create_augroup("on_attach", { clear = true })
 local on_attach = function(client, bufnr)
 	-- Use omnicomplete with LSP
 	if client.supports_method("textDocument/completion") then
-		vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-		vim.keymap.set("i", "<c-space>", "<c-x><c-o>")
-		-- https://github.com/vim/vim/issues/1653
-		vim.keymap.set("i", "<cr>", function()
-			if vim.fn.pumvisible() == 1 and vim.fn.complete_info()["selected"] == -1 then
-				return "<c-e><cr>"
-			else
-				return "<cr>"
-			end
-		end, { expr = true })
-		-- Auto-complete
-		vim.api.nvim_create_autocmd("TextChangedI", {
-			group = "on_attach",
-			callback = function()
-				if vim.opt_local.omnifunc:get() == "" then return end
-				if vim.g.pum_timer then vim.fn.timer_stop(vim.g.pum_timer) end
-				if
-					vim.fn
-						.getline(".")
-						:sub(vim.fn.col(".") - 1, vim.fn.col(".") - 1)
-						:match("[%w_.]")
-				then
-					vim.g.pum_timer = vim.fn.timer_start(400, function()
-						if vim.fn.mode():match("^[^i]") then return end
-						vim.api.nvim_feedkeys("", "n", true)
-					end)
-				end
-			end,
-		})
+		require("lsp.completion").setup(bufnr)
 	end
 
 	-- Format on save
