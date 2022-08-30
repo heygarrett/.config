ccfb6acd1197f64dd787c481eb09c87795447ed8
Garrett Oreilly <hey@heygarrett.com>
2022-08-30 08:55:55 -0400

    feat(null-ls): only use local installation of prettier

--- nvim/lua/config/null-ls.lua
+++ nvim/lua/config/null-ls.lua
@@ -11,8 +11,10 @@ return {
 				null_ls.builtins.code_actions.eslint_d,
 				null_ls.builtins.diagnostics.eslint_d,
 				null_ls.builtins.diagnostics.fish,
-				null_ls.builtins.formatting.prettierd,
 				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettierd.with({
+					env = { PRETTIERD_LOCAL_PRETTIER_ONLY = true },
+				}),
 			},
 			on_attach = require("lsp.on_attach"),
 		})
