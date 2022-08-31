ea44c843936011e3722eb5d6f281b349cdbf7288
Garrett Oreilly <hey@heygarrett.com>
2022-08-31 12:30:06 -0400

    feat(ssh): use 1password for ssh

--- fish/config.fish
+++ fish/config.fish
@@ -3,3 +3,6 @@ if not functions -q fisher
 end
 starship init fish | source
 set -gx GPG_TTY (tty)
+if test -e ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+	set -x SSH_AUTH_SOCK ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+end
