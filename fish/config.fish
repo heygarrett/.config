e4dcd53dab4d27df5104ae54fa987230455be2c5
Garrett Oreilly <hey@heygarrett.com>
2022-09-01 22:40:44 -0400

    fix(config.fish): remove faulty installation function for fisher

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,6 @@
-if not functions -q fisher
-	curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-end
-starship init fish | source
 set -gx GPG_TTY (tty)
 set --local op_ssh ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
 if test -e "$op_ssh"
 	set -x SSH_AUTH_SOCK "$op_ssh"
 end
+starship init fish | source
