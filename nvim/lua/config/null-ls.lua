7461e66acb280f94e1f859a4b1db60b34f933d28
Garrett Oreilly <hey@heygarrett.com>
2022-09-04 12:29:17 -0400

    feat(null-ls): add built-in for todo_comments diagnostics
    error: FIX, FIXME, BUG, ISSUE; warning: HACK; info: TODO, PERF, OPTIMIZE; hint: NOTE, INFO

--- nvim/lua/config/null-ls.lua
+++ nvim/lua/config/null-ls.lua
@@ -1,6 +1,10 @@
 return {
 	"jose-elias-alvarez/null-ls.nvim",
-	requires = "nvim-lua/plenary.nvim",
+	requires = {
+		"nvim-lua/plenary.nvim",
+		-- treesitter needed for todo_comments
+		"nvim-treesitter/nvim-treesitter",
+	},
 	config = function()
 		local loaded, null_ls = pcall(require, "null-ls")
 		if not loaded then return end
@@ -11,6 +15,7 @@ return {
 				null_ls.builtins.code_actions.eslint_d,
 				null_ls.builtins.diagnostics.eslint_d,
 				null_ls.builtins.diagnostics.fish,
+				null_ls.builtins.diagnostics.todo_comments,
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettierd.with({
 					env = { PRETTIERD_LOCAL_PRETTIER_ONLY = true },
