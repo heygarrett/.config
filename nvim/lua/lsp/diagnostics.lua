07e2e1e32c4599e7fe8e2c9d38fadeab245f2519
Garrett Oreilly <hey@heygarrett.com>
2022-09-20 01:07:26 -0400

    feat(diagnostics): use virtual text highlight group for diagnostics instead of diagnostic underline

--- nvim/lua/lsp/diagnostics.lua
+++ nvim/lua/lsp/diagnostics.lua
@@ -1,11 +1,16 @@
 for _, type in ipairs({ "Error", "Warn", "Hint", "Info" }) do
 	local hl = "DiagnosticSign" .. type
 	vim.fn.sign_define(hl, { text = "", texthl = hl, numhl = hl })
-	vim.cmd.highlight({ "DiagnosticUnderline" .. type, "gui=underline" })
+	vim.api.nvim_set_hl(
+		0,
+		"DiagnosticUnderline" .. type,
+		vim.api.nvim_get_hl_by_name("DiagnosticVirtualText" .. type, {})
+	)
+	vim.cmd.highlight({ "DiagnosticUnderline" .. type, "gui=NONE" })
 end
 
 vim.diagnostic.config({
-	virtual_text = true,
+	virtual_text = false,
 	float = { source = "always" },
 })
 
