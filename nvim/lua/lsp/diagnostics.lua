c10c295e716fc9b665d669653d415bbda737fe97
Garrett Oreilly <hey@heygarrett.com>
2022-09-21 01:04:42 -0400

    refactor(diagnostics): remove unused fields for diagnostic sign definition

--- nvim/lua/lsp/diagnostics.lua
+++ nvim/lua/lsp/diagnostics.lua
@@ -5,7 +5,7 @@ vim.diagnostic.config({
 
 for _, type in ipairs({ "Error", "Warn", "Hint", "Info" }) do
 	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = "", texthl = hl, numhl = hl })
+	vim.fn.sign_define(hl, { numhl = hl })
 	vim.cmd.highlight({
 		args = {
 			"DiagnosticVirtualText" .. type,
