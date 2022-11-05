2e9fcdf01942274ccc2e35193da0562d22f3ad5d
Garrett Oreilly <hey@heygarrett.com>
2022-11-05 09:50:41 -0400

    feat(codewindow): use default keymaps

--- nvim/lua/config/codewindow.lua
+++ nvim/lua/config/codewindow.lua
@@ -5,7 +5,6 @@ return {
 		if not loaded then return end
 
 		codewindow.setup()
-		vim.keymap.set("n", "<c-w>m", codewindow.toggle_minimap)
-		vim.keymap.set("n", "<c-w>M", codewindow.toggle_focus)
+		codewindow.apply_default_keybinds()
 	end,
 }
