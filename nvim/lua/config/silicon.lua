a6bc5d8af176195e8c6c670acb9e3b8791845dee
Garrett Oreilly <garrett.oreilly@agilebits.com>
2022-11-08 08:31:08 -0500

    fix(silicon.lua): use newer cmdline function

--- nvim/lua/config/silicon.lua
+++ nvim/lua/config/silicon.lua
@@ -5,15 +5,15 @@ return {
 		if not loaded then return end
 
 		silicon.setup({
-			theme = "Dracula",
 			font = "Monocraft",
-			padHoriz = "30",
-			padVert = "30",
+			padHoriz = "20",
+			padVert = "20",
+			gobble = true,
 		})
 
 		vim.api.nvim_create_user_command(
 			"Silicon",
-			function(t) silicon.visualise_api({ to_clip = t.args ~= "save" }) end,
+			function(t) silicon.visualise_cmdline({ to_clip = t.args ~= "save" }) end,
 			{ range = true, nargs = "?" }
 		)
 	end,
