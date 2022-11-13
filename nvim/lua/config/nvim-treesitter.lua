0696e64cf00a36ab0e0ff90716dd82f88a29a1cf
Garrett Oreilly <hey@heygarrett.com>
2022-11-12 20:10:33 -0500

    feat(treesitter): add pcalls for nvim-treesitter plugins

--- nvim/lua/config/nvim-treesitter.lua
+++ nvim/lua/config/nvim-treesitter.lua
@@ -1,47 +1,59 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	requires = {
+	{
 		"nvim-treesitter/nvim-treesitter-context",
-		"nvim-treesitter/nvim-treesitter-textobjects",
+		config = function()
+			local loaded, _ = pcall(require, "treesitter-context")
+			if not loaded then return end
+		end,
 	},
-	run = function()
-		if package.loaded["nvim-treesitter.configs"] then vim.cmd.TSUpdate() end
-	end,
-	config = function()
-		local loaded, treesitter = pcall(require, "nvim-treesitter.configs")
-		if not loaded then return end
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		config = function()
+			local loaded, _ = pcall(require, "nvim-treesitter-textobjects")
+			if not loaded then return end
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			if package.loaded["nvim-treesitter.configs"] then vim.cmd.TSUpdate() end
+		end,
+		config = function()
+			local loaded, treesitter = pcall(require, "nvim-treesitter.configs")
+			if not loaded then return end
 
-		treesitter.setup({
-			auto_install = true,
-			highlight = {
-				enable = true,
-				disable = { "make" },
-			},
-			textobjects = {
-				select = {
+			treesitter.setup({
+				auto_install = true,
+				highlight = {
 					enable = true,
-					lookahead = true,
-					keymaps = {
-						["al"] = "@loop.outer",
-						["il"] = "@loop.inner",
-						["aC"] = "@class.outer",
-						["iC"] = "@class.inner",
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
-						["ac"] = "@conditional.outer",
-						["ic"] = "@conditional.inner",
+					disable = { "make" },
+				},
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true,
+						keymaps = {
+							["al"] = "@loop.outer",
+							["il"] = "@loop.inner",
+							["aC"] = "@class.outer",
+							["iC"] = "@class.inner",
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@conditional.outer",
+							["ic"] = "@conditional.inner",
+						},
+					},
+					swap = {
+						enable = true,
+						swap_next = {
+							["<leader>a"] = "@parameter.inner",
+						},
+						swap_previous = {
+							["<leader>A"] = "@parameter.inner",
+						},
 					},
 				},
-				swap = {
-					enable = true,
-					swap_next = {
-						["<leader>a"] = "@parameter.inner",
-					},
-					swap_previous = {
-						["<leader>A"] = "@parameter.inner",
-					},
-				},
-			},
-		})
-	end,
+			})
+		end,
+	},
 }
