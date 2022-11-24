e938f983d36f7bbd3674e85b6a396bb3350fff72
Garrett Oreilly <hey@heygarrett.com>
2022-11-24 12:39:50 -0500

    fix(catppuccin.lua): increase contrast on line numbers and comments

--- nvim/lua/config/catppuccin.lua
+++ nvim/lua/config/catppuccin.lua
@@ -5,9 +5,15 @@ return {
 		local loaded, catppuccin = pcall(require, "catppuccin")
 		if not loaded then return end
 
+		local palette = require("catppuccin.palettes").get_palette()
 		catppuccin.setup({
 			flavour = "mocha",
 			no_italic = true,
+			color_overrides = {
+				all = {
+					surface1 = palette.surface2,
+				},
+			},
 		})
 		vim.cmd.colorscheme({
 			args = { "catppuccin" },
