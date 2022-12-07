259456c79cae5936e7d894171704a9684de2bd0f
Garrett Oreilly <garrett.oreilly@agilebits.com>
2022-12-07 13:48:46 -0500

    refactor(formatting.lua): simplify user command and autocmd for LSP formatting

--- nvim/lua/lsp/formatting.lua
+++ nvim/lua/lsp/formatting.lua
@@ -1,44 +1,34 @@
 local M = {}
 
-local function formatting_conditions(client)
-	local disabled_for = {
-		jsonls = true,
-		sumneko_lua = true,
-		tsserver = true,
-	}
-	return not disabled_for[client.name]
-end
-
-local function enforce_tabs()
-	local tabstop = vim.go.tabstop
-	vim.o.tabstop = 2
-	vim.cmd.retab({
-		bang = true,
-	})
-	vim.o.tabstop = tabstop
-end
-
 M.setup = function(bufnr)
-	-- User command
+	-- user command: Format
 	vim.api.nvim_buf_create_user_command(bufnr, "Format", function()
+		-- Disable formatting for specific servers
 		vim.lsp.buf.format({
-			async = true,
-			filter = function(client) return formatting_conditions(client) end,
+			filter = function(client)
+				local disabled_for = {
+					jsonls = true,
+					sumneko_lua = true,
+					tsserver = true,
+				}
+				return not disabled_for[client.name]
+			end,
 		})
-		if not vim.o.expandtab then enforce_tabs() end
+		-- Retab after formatting
+		vim.bo.tabstop = 2
+		vim.cmd.retab({
+			bang = true,
+		})
+		vim.bo.tabstop = vim.go.tabstop
 	end, {})
-	-- Format on save
+
+	-- autocmd: Format on save
 	vim.api.nvim_create_augroup("formatting", { clear = false })
 	vim.api.nvim_clear_autocmds({ group = "formatting", buffer = bufnr })
 	vim.api.nvim_create_autocmd("BufWritePre", {
 		group = "formatting",
 		buffer = bufnr,
-		callback = function()
-			vim.lsp.buf.format({
-				filter = function(client) return formatting_conditions(client) end,
-			})
-			if not vim.o.expandtab then enforce_tabs() end
-		end,
+		callback = function() vim.cmd.Format() end,
 	})
 end
 
