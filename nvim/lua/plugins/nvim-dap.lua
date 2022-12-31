8970d80427c754d3b381276bb472e97f532f5fec
Garrett Oreilly <hey@heygarrett.com>
2022-12-31 15:45:05 -0500

    fix(dap): improve config

--- nvim/lua/plugins/nvim-dap.lua
+++ nvim/lua/plugins/nvim-dap.lua
@@ -1,12 +1,11 @@
+local dap = function() return require("dap") end
+
 return {
 	"mfussenegger/nvim-dap",
-	config = function()
-		local loaded, dap = pcall(require, "dap")
-		if not loaded then return end
-
-		vim.api.nvim_create_user_command("DBreakpoint", dap.toggle_breakpoint, {})
-		vim.api.nvim_create_user_command("DClear", dap.clear_breakpoints, {})
-		vim.api.nvim_create_user_command("DContinue", dap.continue, {})
-		vim.api.nvim_create_user_command("DRepl", dap.repl.toggle, {})
+	init = function()
+		vim.api.nvim_create_user_command("DBreakpoint", dap().toggle_breakpoint, {})
+		vim.api.nvim_create_user_command("DClear", dap().clear_breakpoints, {})
+		vim.api.nvim_create_user_command("DContinue", dap().continue, {})
+		vim.api.nvim_create_user_command("DRepl", dap().repl.toggle, {})
 	end,
 }
