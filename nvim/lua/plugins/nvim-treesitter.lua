ac0cf819ec148b36ea065580b950f67aa61dc470
Garrett Oreilly <hey@heygarrett.com>
2022-12-31 15:51:05 -0500

    fix(nvim-treesitter): improve config

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,51 +1,35 @@
 return {
-	{
+	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
 		"nvim-treesitter/nvim-treesitter-context",
-		config = function()
-			local loaded, _ = pcall(require, "treesitter-context")
-			if not loaded then return end
-		end,
-	},
-	{
 		"nvim-treesitter/nvim-treesitter-textobjects",
-		config = function()
-			local loaded, _ = pcall(require, "nvim-treesitter-textobjects")
-			if not loaded then return end
-		end,
 	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = function()
-			if package.loaded["nvim-treesitter.configs"] then vim.cmd.TSUpdate() end
-		end,
-		config = function()
-			local loaded, treesitter = pcall(require, "nvim-treesitter.configs")
-			if not loaded then return end
-
-			treesitter.setup({
-				auto_install = true,
-				ignore_install = { "gitcommit" },
-				highlight = {
+	build = function() vim.cmd.TSUpdate() end,
+	lazy = false,
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			auto_install = true,
+			ignore_install = { "gitcommit" },
+			highlight = {
+				enable = true,
+				disable = { "make" },
+			},
+			textobjects = {
+				select = {
 					enable = true,
-					disable = { "make" },
-				},
-				textobjects = {
-					select = {
-						enable = true,
-						lookahead = true,
-						keymaps = {
-							["al"] = "@loop.outer",
-							["il"] = "@loop.inner",
-							["aC"] = "@class.outer",
-							["iC"] = "@class.inner",
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-							["ac"] = "@conditional.outer",
-							["ic"] = "@conditional.inner",
-						},
+					lookahead = true,
+					keymaps = {
+						["al"] = "@loop.outer",
+						["il"] = "@loop.inner",
+						["aC"] = "@class.outer",
+						["iC"] = "@class.inner",
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@conditional.outer",
+						["ic"] = "@conditional.inner",
 					},
 				},
-			})
-		end,
-	},
+			},
+		})
+	end,
 }
