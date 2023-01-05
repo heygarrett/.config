92d21033f2fc7e30f22ff199bcc7bc0026004459
Garrett Oreilly <garrett.oreilly@agilebits.com>
2023-01-04 19:25:56 -0500

    refactor(nvim-lspconfig): replace lsp-zero with server setup via mason-lspconfig

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,35 +2,37 @@ return {
 	"neovim/nvim-lspconfig",
 	dependencies = {
 		"folke/neodev.nvim",
-		"VonHeikemen/lsp-zero.nvim",
 	},
 	lazy = false,
 	config = function()
 		require("neodev").setup()
 
-		local lsp_zero = require("lsp-zero")
-		lsp_zero.set_preferences({
-			suggest_lsp_servers = true,
-			setup_servers_on_start = true,
-			set_lsp_keymaps = false,
-			call_servers = "local",
+		local lspconfig = require("lspconfig")
+		local on_attach = require("lsp.on_attach")
+		require("mason-lspconfig").setup_handlers({
+			function(server_name)
+				lspconfig[server_name].setup({
+					on_attach = on_attach,
+				})
+			end,
+			["sumneko_lua"] = function()
+				lspconfig.sumneko_lua.setup({
+					on_attach = on_attach,
+					settings = {
+						Lua = {
+							completion = {
+								callSnippet = "Replace",
+							},
+							workspace = {
+								checkThirdParty = false,
+							},
+							telemetry = {
+								enable = false,
+							},
+						},
+					},
+				})
+			end,
 		})
-		lsp_zero.on_attach(require("lsp.on_attach"))
-		lsp_zero.configure("sumneko_lua", {
-			settings = {
-				Lua = {
-					completion = {
-						callSnippet = "Replace",
-					},
-					workspace = {
-						checkThirdParty = false,
-					},
-					telemetry = {
-						enable = false,
-					},
-				},
-			},
-		})
-		lsp_zero.setup()
 	end,
 }
