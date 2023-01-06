636748ad5b83910f4eeba7151897a3cebce7621a
Garrett Oreilly <hey@heygarrett.com>
2023-01-06 00:04:56 -0500

    fix(kanagawa): override highlight group for current line number

--- nvim/lua/plugins/kanagawa.lua
+++ nvim/lua/plugins/kanagawa.lua
@@ -2,7 +2,12 @@ return {
 	"rebelot/kanagawa.nvim",
 	lazy = false,
 	config = function()
+		local default_colors = require("kanagawa.colors").setup()
+		local overrides = {
+			CursorLineNR = { fg = default_colors.lightBlue },
+		}
 		require("kanagawa").setup({
+			overrides = overrides,
 			commentStyle = {},
 			keywordStyle = {},
 			statementStyle = {},
