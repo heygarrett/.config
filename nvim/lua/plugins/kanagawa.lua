22693bd9fd7517452d51f08cc70c4fab3ed29caa
Garrett Oreilly <garrett.oreilly@agilebits.com>
2023-01-06 17:51:54 -0500

    fix(kanagawa): match name of highlight group CursorLineNr

--- nvim/lua/plugins/kanagawa.lua
+++ nvim/lua/plugins/kanagawa.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		local default_colors = require("kanagawa.colors").setup()
 		local overrides = {
-			CursorLineNR = { fg = default_colors.lightBlue },
+			CursorLineNr = { fg = default_colors.lightBlue },
 		}
 		require("kanagawa").setup({
 			overrides = overrides,
