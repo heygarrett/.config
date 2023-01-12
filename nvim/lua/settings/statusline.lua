55007d033668e882dc3bc961d7e4ec2c0c6a8d26
Garrett Oreilly <hey@heygarrett.com>
2023-01-11 23:03:58 -0500

    fix(statusline): correctly return only one value from branch name function

--- nvim/lua/settings/statusline.lua
+++ nvim/lua/settings/statusline.lua
@@ -3,7 +3,8 @@
 local function get_branch_name()
 	local branch = vim.fn.system("git branch --show-current 2> /dev/null")
 	if branch ~= "" and not vim.g.launched_by_shell then
-		return branch:gsub("\n", "")
+		local branch_name, _ = branch:gsub("\n", "")
+		return branch_name
 	else
 		return nil
 	end
