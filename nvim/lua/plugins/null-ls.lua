b01a271e093579e20c086b08aecc9dbebd42205c
Garrett Oreilly <hey@heygarrett.com>
2023-01-26 16:40:42 -0500

    feat(null-ls): add fish_indent as a formatter
    Recent changes that retain preferred indentation style when formatting make fish_indent practical and useful for fish scripts indented with tabs.

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -11,6 +11,7 @@ return {
 				null_ls.builtins.diagnostics.eslint_d,
 				null_ls.builtins.diagnostics.fish,
 				null_ls.builtins.diagnostics.todo_comments,
+				null_ls.builtins.formatting.fish_indent,
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.yapf,
 				null_ls.builtins.formatting.prettierd.with({
