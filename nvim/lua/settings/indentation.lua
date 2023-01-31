b846e598231dd9213b63136f3db9b89b8fe7b3ea
Garrett Oreilly <garrett.oreilly@agilebits.com>
2023-01-31 15:38:27 -0500

    fix(indentation): support oil.nvim idiosyncrasy
    https://github.com/stevearc/oil.nvim/issues/49#issuecomment-1407567002

--- nvim/lua/settings/indentation.lua
+++ nvim/lua/settings/indentation.lua
@@ -5,7 +5,7 @@ vim.o.softtabstop = -1
 vim.o.tabstop = 4
 vim.opt.listchars = { lead = "·", tab = "| ", trail = "·" }
 
-vim.api.nvim_create_autocmd("FileType", {
+vim.api.nvim_create_autocmd("BufWinEnter", {
 	group = vim.api.nvim_create_augroup("indentation", { clear = true }),
 	callback = function()
 		-- Override ftplugin indentation settings
