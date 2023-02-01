6e0ef0e3ca11378c439a7d86ec0ea6b0d99d1b50
Garrett Oreilly <garrett.oreilly@agilebits.com>
2023-02-01 10:28:15 -0500

    fix(indentation): stop autocmd from overriding options set by editorconfig

--- nvim/lua/settings/indentation.lua
+++ nvim/lua/settings/indentation.lua
@@ -9,18 +9,23 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
 	group = vim.api.nvim_create_augroup("indentation", { clear = true }),
 	callback = function()
 		-- Override ftplugin indentation settings
-		vim.bo.expandtab = vim.go.expandtab
 		vim.bo.shiftwidth = vim.go.shiftwidth
 		vim.bo.softtabstop = vim.go.softtabstop
-		vim.bo.tabstop = vim.go.tabstop
-		vim.wo.listchars = vim.go.listchars
-		-- Run guess-indent
-		if package.loaded["guess-indent"] then
-			vim.cmd.GuessIndent({
-				args = { "auto_cmd" },
-				mods = { silent = true },
-			})
+
+		if not vim.b.editorconfig or next(vim.b.editorconfig) == nil then
+			-- Override only if not set by editorconfig
+			vim.bo.expandtab = vim.go.expandtab
+			vim.bo.tabstop = vim.go.tabstop
+
+			-- Run guess-indent
+			if package.loaded["guess-indent"] then
+				vim.cmd.GuessIndent({
+					args = { "auto_cmd" },
+					mods = { silent = true },
+				})
+			end
 		end
+
 		-- Set whitespace characters for indentation with spaces
 		if vim.bo.expandtab then
 			vim.opt_local.listchars:append({
