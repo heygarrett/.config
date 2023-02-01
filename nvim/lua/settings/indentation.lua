6cef130c65327d04067e510bbe9f0213097bd639
Garrett Oreilly <garrett.oreilly@agilebits.com>
2023-02-01 13:16:05 -0500

    fix(indentation): always override tabstop when using tabs

--- nvim/lua/settings/indentation.lua
+++ nvim/lua/settings/indentation.lua
@@ -15,7 +15,6 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
 		if not vim.b.editorconfig or next(vim.b.editorconfig) == nil then
 			-- Override only if not set by editorconfig
 			vim.bo.expandtab = vim.go.expandtab
-			vim.bo.tabstop = vim.go.tabstop
 
 			-- Run guess-indent
 			if package.loaded["guess-indent"] then
@@ -26,11 +25,13 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
 			end
 		end
 
-		-- Set whitespace characters for indentation with spaces
 		if vim.bo.expandtab then
+			-- Set whitespace characters for indentation with spaces
 			vim.opt_local.listchars:append({
 				leadmultispace = ":" .. (" "):rep(vim.bo.tabstop - 1),
 			})
+		else
+			vim.bo.tabstop = vim.go.tabstop
 		end
 	end,
 })
