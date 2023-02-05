e8a1188a757471b318ee80e251356288f83f37f3
Garrett Oreilly <garrett.oreilly@agilebits.com>
2023-02-05 15:54:27 -0500

    feat(utils.lua): add user command Chomp to remove leading indentation on copied code

--- nvim/lua/settings/utils.lua
+++ nvim/lua/settings/utils.lua
@@ -14,3 +14,19 @@ vim.api.nvim_create_user_command("Sort", function(t)
 	end)
 	vim.api.nvim_buf_set_lines(0, t.line1 - 1, t.line2, true, line_list)
 end, { range = "%", bang = true })
+
+-- Copy code without leading indents
+vim.api.nvim_create_user_command("Chomp", function(tbl)
+	local line_list = vim.api.nvim_buf_get_lines(0, tbl.line1 - 1, tbl.line2, true)
+	local global_depth = 0
+	for _, line in ipairs(line_list) do
+		local line_depth = (line:match("^%s+") or ""):len()
+		if global_depth == 0 or line_depth < global_depth then
+			global_depth = line_depth
+		end
+	end
+	for index, line in ipairs(line_list) do
+		line_list[index] = line:sub(global_depth + 1)
+	end
+	vim.fn.setreg("+", table.concat(line_list, "\n"))
+end, { range = true })
