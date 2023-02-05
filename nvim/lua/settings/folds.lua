dfd9b6b43e26d29e10f00db1e608a31e893c6383
Garrett Oreilly <garrett.oreilly@agilebits.com>
2023-02-05 09:27:48 -0500

    feat(folds.lua): add preview of first line to fold text

--- nvim/lua/settings/folds.lua
+++ nvim/lua/settings/folds.lua
@@ -1,9 +1,15 @@
 function _G.fold_text()
-	local number_of_lines = vim.v.foldend - vim.v.foldstart + 1
 	local indent_char = vim.bo.expandtab and ":" or "|"
 	local indent_block = indent_char .. (" "):rep(vim.bo.tabstop - 1)
 	local prefix = indent_block:rep(vim.v.foldlevel):gsub(" $", "")
-	return table.concat({ prefix, number_of_lines, "lines folded" }, " ")
+	local number_of_lines = vim.v.foldend - vim.v.foldstart + 1
+	local preview =
+		vim.api.nvim_buf_get_lines(0, vim.v.foldstart - 1, vim.v.foldstart, true)[1]
+	local stripped_preview = preview:gsub("^%s+", "")
+	return table.concat(
+		{ prefix, number_of_lines, "lines folded:", stripped_preview },
+		" "
+	)
 end
 
 vim.o.foldenable = false
