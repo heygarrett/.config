3c69b7a8d6afb5250894a144fa80a6ca7270467f
Garrett Oreilly <garrett.oreilly@agilebits.com>
2023-02-05 18:20:00 -0500

    feat(utils): replace Chomp command with autocmd that asks whether to chomp after yanking

--- nvim/lua/settings/utils.lua
+++ nvim/lua/settings/utils.lua
@@ -15,18 +15,36 @@ vim.api.nvim_create_user_command("Sort", function(t)
 	vim.api.nvim_buf_set_lines(0, t.line1 - 1, t.line2, true, line_list)
 end, { range = "%", bang = true })
 
--- Copy code without leading indents
-vim.api.nvim_create_user_command("Chomp", function(tbl)
-	local line_list = vim.api.nvim_buf_get_lines(0, tbl.line1 - 1, tbl.line2, true)
-	local global_depth = 0
-	for _, line in ipairs(line_list) do
-		local line_depth = (line:match("^%s+") or ""):len()
-		if global_depth == 0 or line_depth < global_depth then
-			global_depth = line_depth
+-- Option to copy code without leading indents
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = vim.api.nvim_create_augroup("utils", { clear = true }),
+	callback = function()
+		if vim.v.operator ~= "y" or vim.v.register ~= "+" then
+			return
 		end
-	end
-	for index, line in ipairs(line_list) do
-		line_list[index] = line:sub(global_depth + 1)
-	end
-	vim.fn.setreg("+", table.concat(line_list, "\n"))
-end, { range = true })
+		local yanked_text = vim.fn.getreg()
+		local global_depth = 0
+		local line_list = {}
+		for line in yanked_text:gmatch("[^\n]+") do
+			local line_depth = (line:match("^%s+") or ""):len()
+			if global_depth == 0 or line_depth < global_depth then
+				global_depth = line_depth
+			end
+			table.insert(line_list, line)
+		end
+
+		local chomp = "no"
+		if global_depth > 0 then
+			vim.ui.input(
+				{ prompt = "Chomp indentation? [y/N] " },
+				function(input) chomp = input end
+			)
+		end
+		if chomp and not chomp:match("^[nN]$") then
+			for index, line in ipairs(line_list) do
+				line_list[index] = line:sub(global_depth + 1)
+			end
+			vim.fn.setreg(vim.v.register, table.concat(line_list, "\n"))
+		end
+	end,
+})
