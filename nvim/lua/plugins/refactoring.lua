984939032f39856cdb87caa5d291257bc8e1dba4
Garrett Oreilly <garrett.oreilly@agilebits.com>
2023-02-12 11:58:54 -0500

    feat(nvim): add plugin refactoring.nvim
    testing it out

---
+++ nvim/lua/plugins/refactoring.lua
@@ -0,0 +1,16 @@
+local refactoring = function() return require("refactoring") end
+
+return {
+	"https://github.com/ThePrimeagen/refactoring.nvim",
+	init = function()
+		vim.api.nvim_create_user_command(
+			"Refactor",
+			refactoring().select_refactor,
+			{ range = true }
+		)
+	end,
+	config = function()
+		vim.o.shiftwidth = vim.o.tabstop
+		refactoring().setup()
+	end,
+}
