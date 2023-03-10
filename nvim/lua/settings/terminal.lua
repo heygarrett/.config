b5e032a3b60e7838bf61923089681643359491a2
Garrett Oreilly <hey@heygarrett.com>
2023-03-09 22:56:51 -0500

    feat(terminal.lua): auto-close terminal windows after process ends

--- nvim/lua/settings/terminal.lua
+++ nvim/lua/settings/terminal.lua
@@ -9,3 +9,13 @@ vim.api.nvim_create_autocmd("TermOpen", {
 		vim.o.number = false
 	end,
 })
+
+vim.api.nvim_create_autocmd("TermClose", {
+	desc = "close terminal when process ends",
+	group = "terminal",
+	callback = function()
+		if vim.bo.buftype == "terminal" then
+			vim.api.nvim_win_close(0, false)
+		end
+	end,
+})
