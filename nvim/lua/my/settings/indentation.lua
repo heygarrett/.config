e8a964655e2c9847822148893f22800559d8927c
Garrett Oreilly <garrett.oreilly@agilebits.com>
2023-04-05 14:10:00 -0400

    refactor(indentation): simplification
    guess-indent now supports editorconfig, so it will only run if
    editorconfig hasn't set options for indentation. That means the
    indentation configuration can be rearranged a bit to make it a bit more
    sensible.

--- nvim/lua/my/settings/indentation.lua
+++ nvim/lua/my/settings/indentation.lua
@@ -5,17 +5,30 @@ vim.o.softtabstop = -1
 vim.o.tabstop = 4
 vim.opt.listchars = { lead = "·", tab = "| ", trail = "·" }
 
+-- Don't run editorconfig automatically
+vim.g.editorconfig_enable = false
+
 vim.api.nvim_create_autocmd("BufWinEnter", {
 	desc = "indentation settings",
 	group = vim.api.nvim_create_augroup("indentation", { clear = true }),
-	callback = function()
+	callback = function(args)
 		-- Override ftplugin indentation settings
 		---@diagnostic disable: undefined-field
 		vim.bo.expandtab = vim.go.expandtab
 		vim.bo.softtabstop = vim.go.softtabstop
 		---@diagnostic enable: undefined-field
 
-		-- Run guess-indent
+		-- Run editorconfig
+		local ec_loaded, editorconfig = pcall(require, "editorconfig")
+		if ec_loaded then
+			editorconfig.config(args.buf)
+		end
+
+		-- Override shiftwidth set by editorconfig
+		---@diagnostic disable-next-line: undefined-field
+		vim.bo.shiftwidth = vim.go.shiftwidth
+
+		-- Run guess-indent (which defers to editorconfig)
 		local gi_loaded, _ = pcall(require, "guess-indent")
 		if gi_loaded then
 			vim.cmd.GuessIndent({
@@ -23,16 +36,6 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
 				mods = { silent = true },
 			})
 		end
-
-		-- Run editorconfig
-		local ec_loaded, editorconfig = pcall(require, "editorconfig")
-		if ec_loaded then
-			editorconfig.config()
-		end
-
-		-- Keep shiftwidth matched to tabstop
-		---@diagnostic disable-next-line: undefined-field
-		vim.bo.shiftwidth = vim.go.shiftwidth
 
 		if vim.bo.expandtab then
 			-- Set whitespace characters for indentation with spaces
