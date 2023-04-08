bc176b0ff71e49c10e835cf4e71b3b80df95c6b7
Garrett Oreilly <garrett.oreilly@agilebits.com>
2023-04-08 11:17:54 -0400

    feat(folding): simplify fold text

--- nvim/lua/my/settings/folding.lua
+++ nvim/lua/my/settings/folding.lua
@@ -1,17 +1,10 @@
 function _G.fold_text()
-	local indent_char = vim.bo.expandtab and ":" or "|"
-	local indent_block = indent_char .. (" "):rep(vim.bo.tabstop - 1)
-	local prefix = indent_block:rep(vim.v.foldlevel):gsub(" $", "")
-	local number_of_lines = vim.v.foldend - vim.v.foldstart + 1
-	local preview =
+	local first_line =
 		vim.api.nvim_buf_get_lines(0, vim.v.foldstart - 1, vim.v.foldstart, true)[1]
-	local stripped_preview = preview:gsub("^%s+", "")
-	return table.concat({
-		prefix,
-		number_of_lines,
-		"lines folded:",
-		stripped_preview,
-	}, " ")
+	local preview = first_line:gsub("\t", (" "):rep(vim.bo.tabstop))
+	local number_of_lines = vim.v.foldend - vim.v.foldstart
+	local foldtext = ("%s +- %d lines below the fold -+"):format(preview, number_of_lines)
+	return foldtext
 end
 
 vim.o.foldenable = false
