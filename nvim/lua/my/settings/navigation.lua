b2380b60f8617f1ddea3af544d17167d917a5c0f
Garrett Oreilly <garrett.oreilly@agilebits.com>
2023-04-26 11:01:26 -0400

    feat(navigation.lua): accordion horizontal splits
    Define an autocommand that expands the current window to max height,
    effectively turning a group of horizontal splits into a vertical
    accordion

--- nvim/lua/my/settings/navigation.lua
+++ nvim/lua/my/settings/navigation.lua
@@ -6,3 +6,17 @@ vim.keymap.set(
 )
 vim.keymap.set("n", "j", "gj", { desc = "go down one display line" })
 vim.keymap.set("n", "k", "gk", { desc = "go up to one display line" })
+
+local group = vim.api.nvim_create_augroup("navigation", { clear = true })
+
+vim.api.nvim_create_autocmd("WinEnter", {
+	group = group,
+	callback = function()
+		if vim.api.nvim_win_get_config(0).relative ~= "" then
+			-- don't resize floating windows
+			return
+		end
+		vim.cmd.resize()
+	end,
+	desc = "turn horizontal splits into a vertical accordion",
+})
