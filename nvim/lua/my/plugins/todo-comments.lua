a3987af5b18dcf11bc5d1fbdf762093f55f88487
Garrett Oreilly <hey@heygarrett.com>
2023-05-04 15:18:08 -0400

    feat(nvim): add plugin todo-comments.nvim

---
+++ nvim/lua/my/plugins/todo-comments.lua
@@ -0,0 +1,25 @@
+local todo_comments = function() return require("todo-comments") end
+
+return {
+	"https://github.com/folke/todo-comments.nvim",
+	dependencies = "https://github.com/nvim-lua/plenary.nvim",
+	init = function()
+		vim.keymap.set(
+			"n",
+			"]t",
+			function() todo_comments().jump_next() end,
+			{ desc = "jump to next todo comment" }
+		)
+		vim.keymap.set(
+			"n",
+			"[t",
+			function() todo_comments().jump_prev() end,
+			{ desc = "jump to previous todo comment" }
+		)
+	end,
+	config = function()
+		require("todo-comments").setup({
+			signs = false,
+		})
+	end,
+}
