8b36a320f6f07a04e17eea6ab8913ff18b1decde
Garrett Oreilly <hey@heygarrett.com>
2023-05-29 15:36:02 -0400

    refactor(fish): move snippets to config

--- fish/conf.d/runtime.fish
+++
@@ -1,7 +0,0 @@
-if test ! -d ~/.runtime
-	mkdir ~/.runtime 2> /dev/null
-		or echo "Password required to create ~/.runtime"
-		and sudo mkdir ~/.runtime
-		and sudo chown $USER ~/.runtime
-	chmod 0700 ~/.runtime
-end
