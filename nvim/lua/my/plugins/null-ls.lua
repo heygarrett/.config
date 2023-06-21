5a32b50c53b80182a082093bfb9deb86576d223b
Garrett Oreilly <hey@heygarrett.com>
2023-06-21 19:08:54 -0400

    feat(nvim): add conditions to null-ls sources

--- nvim/lua/my/plugins/null-ls.lua
+++ nvim/lua/my/plugins/null-ls.lua
@@ -10,11 +10,18 @@ return {
 				null_ls.builtins.diagnostics.eslint_d,
 				null_ls.builtins.diagnostics.fish,
 				null_ls.builtins.formatting.fish_indent,
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.yapf,
 				null_ls.builtins.formatting.prettierd.with({
 					env = { PRETTIERD_LOCAL_PRETTIER_ONLY = true },
+					condition = function(utils)
+						return utils.root_has_file_matches("prettier")
+					end,
 				}),
+				null_ls.builtins.formatting.stylua.with({
+					condition = function(utils)
+						return utils.root_has_file_matches("stylua")
+					end,
+				}),
+				null_ls.builtins.formatting.yapf,
 			},
 		})
 	end,
