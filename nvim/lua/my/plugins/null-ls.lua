9cf7435887e7e1538f34885781c3cb728a225835
Garrett Oreilly <garrett.oreilly@agilebits.com>
2023-06-22 13:41:15 -0400

    fix(nvim): set null-ls root directory to cwd

--- nvim/lua/my/plugins/null-ls.lua
+++ nvim/lua/my/plugins/null-ls.lua
@@ -5,6 +5,8 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			debug = false,
+			-- set root directory to cwd
+			root_dir = function() return nil end,
 			sources = {
 				null_ls.builtins.code_actions.eslint_d,
 				null_ls.builtins.diagnostics.eslint_d,
