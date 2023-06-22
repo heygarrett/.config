a92fdfcb2326e7e520ad16cccf24de3cc492f6ab
Garrett Oreilly <garrett.oreilly@agilebits.com>
2023-06-22 13:41:46 -0400

    feat(nvim): add conditions to null-ls sources

--- nvim/lua/my/plugins/null-ls.lua
+++ nvim/lua/my/plugins/null-ls.lua
@@ -8,8 +8,16 @@ return {
 			-- set root directory to cwd
 			root_dir = function() return nil end,
 			sources = {
-				null_ls.builtins.code_actions.eslint_d,
-				null_ls.builtins.diagnostics.eslint_d,
+				null_ls.builtins.code_actions.eslint_d.with({
+					condition = function(utils)
+						return utils.root_has_file_matches("eslint")
+					end,
+				}),
+				null_ls.builtins.diagnostics.eslint_d.with({
+					condition = function(utils)
+						return utils.root_has_file_matches("eslint")
+					end,
+				}),
 				null_ls.builtins.diagnostics.fish,
 				null_ls.builtins.formatting.fish_indent,
 				null_ls.builtins.formatting.prettierd.with({
