cf5a58809a78391619d9a2d9270b9be119cc0876
Garrett Oreilly <hey@heygarrett.com>
2023-06-29 22:49:15 -0400

    format(null-ls): break up builtins

--- nvim/lua/my/plugins/null-ls.lua
+++ nvim/lua/my/plugins/null-ls.lua
@@ -8,17 +8,20 @@ return {
 			-- set root directory to cwd
 			root_dir = function() return nil end,
 			sources = {
+				-- code actions
 				null_ls.builtins.code_actions.eslint_d.with({
 					condition = function(utils)
 						return utils.root_has_file_matches("eslint")
 					end,
 				}),
+				-- diagnostics
 				null_ls.builtins.diagnostics.eslint_d.with({
 					condition = function(utils)
 						return utils.root_has_file_matches("eslint")
 					end,
 				}),
 				null_ls.builtins.diagnostics.fish,
+				-- formatting
 				null_ls.builtins.formatting.fish_indent,
 				null_ls.builtins.formatting.prettierd.with({
 					env = { PRETTIERD_LOCAL_PRETTIER_ONLY = true },
