903c00f4deb497cb677e221f5d91f389992b3da2
Garrett Oreilly <hey@heygarrett.com>
2023-06-29 22:50:23 -0400

    feat(nvim): add user commands for xbase

--- nvim/lua/my/plugins/xbase.lua
+++ nvim/lua/my/plugins/xbase.lua
@@ -4,6 +4,25 @@ return {
 	ft = "swift",
 	config = function()
 		require("lspconfig").sourcekit.setup({})
-		require("xbase").setup()
+		require("xbase").setup({
+			mappings = { enable = false },
+		})
+
+		local pickers = require("xbase.pickers")
+		vim.api.nvim_create_user_command(
+			"Build",
+			function() pickers.build() end,
+			{ desc = "xbase build" }
+		)
+		vim.api.nvim_create_user_command(
+			"Run",
+			function() pickers.run() end,
+			{ desc = "xbase run" }
+		)
+		vim.api.nvim_create_user_command(
+			"Watch",
+			function() pickers.watch({}) end,
+			{ desc = "xbase watch" }
+		)
 	end,
 }
