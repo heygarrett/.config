736a0064bd1301d81de801068916339dc3b01cf1
Garrett Oreilly <hey@heygarrett.com>
2023-10-18 22:48:01 -0400

    refactor(mini.pick): dry up commit pickers

--- nvim/lua/my/plugins/mini-pick.lua
+++ nvim/lua/my/plugins/mini-pick.lua
@@ -27,11 +27,32 @@ local get_commits = function(ref, file)
 end
 
 ---@param item string
+---@return string
+local get_commit_hash = function(item) return item:match("^%S+") end
+
+---@param item string
 local copy_commit_hash = function(item)
-	local commit = item:match("[^ ]+")
-	vim.fn.setreg("+", commit)
+	local commit_hash = get_commit_hash(item)
+	vim.fn.setreg("+", commit_hash)
 	---@diagnostic disable: param-type-mismatch
-	vim.defer_fn(function() vim.notify(commit .. " copied to clipboard!") end, 500)
+	vim.defer_fn(function() vim.notify(commit_hash .. " copied to clipboard!") end, 500)
+end
+
+---@param buf_id number
+---@param item string
+---@param file string|nil
+local preview_commit = function(buf_id, item, file)
+	vim.bo[buf_id].syntax = "diff"
+	local commit_hash = get_commit_hash(item)
+	---@type string[]
+	local git_cmd
+	if file then
+		git_cmd = { "git", "diff", commit_hash, "--", file }
+	else
+		git_cmd = { "git", "show", commit_hash }
+	end
+	local lines = vim.split(vim.fn.system(git_cmd), "\n", { trimempty = true })
+	vim.api.nvim_buf_set_lines(buf_id, 0, -1, false, lines)
 end
 
 return {
@@ -41,27 +62,14 @@ return {
 		pick.setup()
 
 		vim.api.nvim_create_user_command("BCommits", function(opts)
-			local relative_file = vim.fn.expand("%:.")
+			local buffer_file = vim.fn.expand("%:.")
 			pick.start({
 				source = {
 					name = "BCommits",
-					items = get_commits(opts.args, relative_file),
+					items = get_commits(opts.args, buffer_file),
 					choose = copy_commit_hash,
 					preview = function(buf_id, item)
-						vim.bo[buf_id].filetype = "diff"
-						local hash = item:match("[^ ]+")
-						local lines = vim.split(
-							vim.fn.system({
-								"git",
-								"diff",
-								hash,
-								"--",
-								relative_file,
-							}),
-							"\n",
-							{ trimempty = true }
-						)
-						vim.api.nvim_buf_set_lines(buf_id, 0, -1, false, lines)
+						preview_commit(buf_id, item, buffer_file)
 					end,
 				},
 			})
@@ -71,29 +79,24 @@ return {
 			desc = "mini.pick: buffer commits",
 		})
 
-		vim.api.nvim_create_user_command("Commits", function(opts)
-			pick.start({
-				source = {
-					items = get_commits(opts.args),
-					name = "Commits",
-					choose = copy_commit_hash,
-					preview = function(buf_id, item)
-						vim.bo[buf_id].filetype = "diff"
-						local hash = item:match("[^ ]+")
-						local lines = vim.split(
-							vim.fn.system({ "git", "show", hash }),
-							"\n",
-							{ trimempty = true }
-						)
-						vim.api.nvim_buf_set_lines(buf_id, 0, -1, false, lines)
-					end,
-				},
-			})
-		end, {
-			nargs = "?",
-			complete = helpers.get_branches,
-			desc = "mini.pick: commits",
-		})
+		vim.api.nvim_create_user_command(
+			"Commits",
+			function(opts)
+				pick.start({
+					source = {
+						items = get_commits(opts.args),
+						name = "Commits",
+						choose = copy_commit_hash,
+						preview = preview_commit,
+					},
+				})
+			end,
+			{
+				nargs = "?",
+				complete = helpers.get_branches,
+				desc = "mini.pick: commits",
+			}
+		)
 
 		vim.api.nvim_create_user_command("Find", function()
 			vim.fn.system({ "git", "rev-parse", "--is-inside-work-tree" })
