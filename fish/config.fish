55fc22c8d711656731c6cbe72f4628473c41faff
Garrett Oreilly <hey@heygarrett.com>
2023-10-30 01:05:40 -0400

    refactor(fish): do most of config only in login shell

--- fish/config.fish
+++ fish/config.fish
@@ -6,34 +6,34 @@ if status is-login
 		/opt/homebrew/bin \
 		/opt/homebrew/sbin \
 		/opt/homebrew/opt/fzf/bin
-end
 
-# create XDG_RUNTIME_DIR
-if test ! -d ~/.runtime
-	mkdir ~/.runtime 2>/dev/null
-	or echo "Password required to create ~/.runtime"
-	and sudo mkdir ~/.runtime
-	and sudo chown $USER ~/.runtime
-	chmod 0700 ~/.runtime
-end
+	# create XDG_RUNTIME_DIR
+	if test ! -d ~/.runtime
+		mkdir ~/.runtime 2>/dev/null
+		or echo "Password required to create ~/.runtime"
+		and sudo mkdir ~/.runtime
+		and sudo chown $USER ~/.runtime
+		chmod 0700 ~/.runtime
+	end
 
-# install wezterm terminfo
-if not infocmp wezterm &>/dev/null
-	set --local tempfile $(mktemp)
-	set --local url $(
+	# install wezterm terminfo
+	if not infocmp wezterm &>/dev/null
+		set --local tempfile $(mktemp)
+		set --local url $(
 		string join "" \
 		"https://raw.githubusercontent.com/" \
 		"wez/wezterm/master/termwiz/data/wezterm.terminfo"
 	)
-	curl -o $tempfile $url &>/dev/null
-	tic -x -o ~/.terminfo $tempfile
-	rm $tempfile
-end
+		curl -o $tempfile $url &>/dev/null
+		tic -x -o ~/.terminfo $tempfile
+		rm $tempfile
+	end
 
-# set up 1Password ssh agent
-set --local op_ssh ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
-if test -e "$op_ssh"
-	set -x SSH_AUTH_SOCK "$op_ssh"
+	# set up 1Password ssh agent
+	set --local op_ssh ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+	if test -e "$op_ssh"
+		set -x SSH_AUTH_SOCK "$op_ssh"
+	end
 end
 
 # extensions
