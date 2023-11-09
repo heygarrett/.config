ee3022bc50f48ed6cd6b858d51eff54e0d93e445
Garrett Oreilly <hey@heygarrett.com>
2023-11-09 00:36:09 -0500

    refactor(mason-lspconfig): disable auto-installation of LSP servers for new filetypes

--- nvim/lua/my/plugins/mason.lua
+++ nvim/lua/my/plugins/mason.lua
@@ -6,53 +6,6 @@ return {
 	},
 	{
 		"https://github.com/williamboman/mason-lspconfig.nvim",
-		config = function()
-			local mason_lspconfig = require("mason-lspconfig")
-			mason_lspconfig.setup()
-			local ignored_filetypes = setmetatable(
-				{ "git", "haskell", "text" },
-				{
-					__index = function(tbl, key)
-						for _, ft in ipairs(tbl) do
-							if key:match("^" .. ft) then
-								return true
-							end
-						end
-						return false
-					end,
-				}
-			)
-			local group =
-				vim.api.nvim_create_augroup("mason-lspconfig", { clear = true })
-			vim.api.nvim_create_autocmd("FileType", {
-				desc = "Auto-install language servers for new file types",
-				group = group,
-				callback = function(t)
-					if ignored_filetypes[t.match] then
-						return
-					end
-					if vim.bo[t.buf].buftype ~= "" then
-						return
-					end
-					local available_servers =
-						mason_lspconfig.get_available_servers({
-							filetype = t.match,
-						})
-					if #available_servers == 0 then
-						return
-					end
-					local installed_servers =
-						mason_lspconfig.get_installed_servers()
-					for _, available in ipairs(available_servers) do
-						for _, installed in ipairs(installed_servers) do
-							if available == installed then
-								return
-							end
-						end
-					end
-					vim.defer_fn(vim.cmd.LspInstall, 500)
-				end,
-			})
-		end,
+		config = true,
 	},
 }
