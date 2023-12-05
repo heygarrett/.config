5193ef38e24cc467d63e27442439c09deead8b35
Garrett Oreilly <garrett.oreilly@agilebits.com>
2023-12-05 18:02:42 -0500

    refactor(fish): sort binds

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,8 +1,8 @@
 function fish_user_key_bindings
 	bind \ce edit_command_buffer
 	bind -M insert \ce edit_command_buffer
-	bind -M insert \cy accept-autosuggestion
 	bind -M insert \cf forward-bigword
+	bind -M insert \cy accept-autosuggestion
 end
 
 fzf_key_bindings
