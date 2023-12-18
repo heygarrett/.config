fc9b0c8a3e130356d72a19853ac849192622811f
Garrett Oreilly <hey@heygarrett.com>
2023-12-18 13:31:20 -0500

    fix(nvim): load options.lua before other settings

--- nvim/lua/my/settings/init.lua
+++ nvim/lua/my/settings/init.lua
@@ -1,9 +1,10 @@
+require("my.settings.options")
+
 require("my.settings.lsp")
 require("my.settings.commands")
 require("my.settings.folding")
 require("my.settings.indentation")
 require("my.settings.navigation")
-require("my.settings.options")
 require("my.settings.sessions")
 require("my.settings.statusline")
 require("my.settings.terminal")
