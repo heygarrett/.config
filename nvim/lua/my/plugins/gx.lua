1163bcb1656643f302bb3a624cc89c1d0f0c1d03
Garrett Oreilly <hey@heygarrett.com>
2024-01-16 23:15:27 -0500

    fix(gx.nvim): define keymap in config

--- nvim/lua/my/plugins/gx.lua
+++ nvim/lua/my/plugins/gx.lua
@@ -1,7 +1,13 @@
 return {
 	"https://github.com/chrishrb/gx.nvim",
 	lazy = true,
-	keys = "gx",
+	keys = {
+		{
+			"gx",
+			function() vim.cmd.Browse() end,
+			mode = { "n", "x" },
+		},
+	},
 	dependencies = { "https://github.com/nvim-lua/plenary.nvim" },
 	config = function()
 		require("gx").setup({
