08e0174c1bd92951fcf634399ca5cebfd0db3fb0
Garrett Oreilly <hey@heygarrett.com>
2024-03-25 10:56:05 -0400

    refactor(nvim-treesitter): use opts table

--- nvim/lua/my/plugins/nvim-treesitter.lua
+++ nvim/lua/my/plugins/nvim-treesitter.lua
@@ -4,31 +4,30 @@ return {
 		"https://github.com/nvim-treesitter/nvim-treesitter-textobjects",
 	},
 	build = function() vim.cmd.TSUpdate() end,
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			auto_install = true,
-			ensure_installed = { "markdown_inline" },
-			ignore_install = { "diff", "gitcommit" },
-			highlight = {
+	opts = {
+		auto_install = true,
+		ensure_installed = { "markdown_inline" },
+		ignore_install = { "diff", "gitcommit" },
+		highlight = {
+			enable = true,
+			disable = { "make" },
+		},
+		textobjects = {
+			select = {
 				enable = true,
-				disable = { "make" },
-			},
-			textobjects = {
-				select = {
-					enable = true,
-					lookahead = true,
-					keymaps = {
-						["al"] = "@loop.outer",
-						["il"] = "@loop.inner",
-						["aC"] = "@class.outer",
-						["iC"] = "@class.inner",
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
-						["ac"] = "@conditional.outer",
-						["ic"] = "@conditional.inner",
-					},
+				lookahead = true,
+				keymaps = {
+					["al"] = "@loop.outer",
+					["il"] = "@loop.inner",
+					["aC"] = "@class.outer",
+					["iC"] = "@class.inner",
+					["af"] = "@function.outer",
+					["if"] = "@function.inner",
+					["ac"] = "@conditional.outer",
+					["ic"] = "@conditional.inner",
 				},
 			},
-		})
-	end,
+		},
+	},
+	config = function(_, opts) require("nvim-treesitter.configs").setup(opts) end,
 }
