ccb8a1d81b2aed4746e236f7949a2cb510933ec2
Garrett Oreilly <hey@heygarrett.com>
2024-03-25 11:34:03 -0400

    refactor(oil.nvim): use opts table

--- nvim/lua/my/plugins/oil.lua
+++ nvim/lua/my/plugins/oil.lua
@@ -1,21 +1,21 @@
 return {
 	"https://github.com/stevearc/oil.nvim",
-	config = function()
+	opts = {
+		view_options = {
+			show_hidden = true,
+			is_always_hidden = function(name) return name == ".." end,
+		},
+		keymaps = {
+			["<C-h>"] = false,
+			["<C-l>"] = false,
+			["<C-r>"] = "actions.refresh",
+			["<C-s>"] = "actions.select_split",
+			["<C-v>"] = "actions.select_vsplit",
+		},
+	},
+	config = function(_, opts)
 		local oil = require("oil")
-		oil.setup({
-			view_options = {
-				show_hidden = true,
-				is_always_hidden = function(name) return name == ".." end,
-			},
-			keymaps = {
-				["<C-h>"] = false,
-				["<C-l>"] = false,
-				["<C-r>"] = "actions.refresh",
-				["<C-s>"] = "actions.select_split",
-				["<C-v>"] = "actions.select_vsplit",
-			},
-		})
-
+		oil.setup(opts)
 		vim.cmd.delcommand("Oil")
 		vim.api.nvim_create_user_command(
 			"Ex",
