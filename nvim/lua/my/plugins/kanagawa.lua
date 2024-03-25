9f067884ac3016c09a39806b7ab4e6de49a31d9e
Garrett Oreilly <hey@heygarrett.com>
2024-03-25 11:26:23 -0400

    refactor(kanagawa.nvim): use opts table

--- nvim/lua/my/plugins/kanagawa.lua
+++ nvim/lua/my/plugins/kanagawa.lua
@@ -1,25 +1,26 @@
 return {
 	"https://github.com/rebelot/kanagawa.nvim",
-	config = function()
-		require("kanagawa").setup({
-			commentStyle = { italic = false },
-			keywordStyle = { italic = false },
-			statementStyle = { bold = false },
-			colors = {
-				theme = { all = { ui = { bg_gutter = "none" } } },
-			},
-			overrides = function(colors)
-				return {
-					Boolean = { bold = false },
-					CursorLineNr = { fg = colors.palette.lightBlue },
-					["@keyword.operator"] = { bold = false },
-					["@keyword.return"] = { fg = colors.theme.syn.keyword },
-					["@lsp.typemod.function.readonly"] = { bold = false },
-					["@string.escape"] = { bold = false },
-					["@variable.builtin"] = { italic = false },
-				}
-			end,
-		})
+	opts = {
+		commentStyle = { italic = false },
+		keywordStyle = { italic = false },
+		statementStyle = { bold = false },
+		colors = {
+			theme = { all = { ui = { bg_gutter = "none" } } },
+		},
+		overrides = function(colors)
+			return {
+				Boolean = { bold = false },
+				CursorLineNr = { fg = colors.palette.lightBlue },
+				["@keyword.operator"] = { bold = false },
+				["@keyword.return"] = { fg = colors.theme.syn.keyword },
+				["@lsp.typemod.function.readonly"] = { bold = false },
+				["@string.escape"] = { bold = false },
+				["@variable.builtin"] = { italic = false },
+			}
+		end,
+	},
+	config = function(_, opts)
+		require("kanagawa").setup(opts)
 		vim.cmd.colorscheme({
 			args = { "kanagawa" },
 		})
