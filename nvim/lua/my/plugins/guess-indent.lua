794f1a6fc9c7c0574ece5af719570b272ebbc1ef
Garrett Oreilly <hey@heygarrett.com>
2024-03-25 11:17:27 -0400

    refactor(guess-indent): use opts table

--- nvim/lua/my/plugins/guess-indent.lua
+++ nvim/lua/my/plugins/guess-indent.lua
@@ -1,15 +1,13 @@
 return {
 	"https://github.com/NMAC427/guess-indent.nvim",
 	lazy = true,
-	config = function()
-		require("guess-indent").setup({
-			auto_cmd = false,
-			override_editorconfig = true,
-			filetype_exclude = {
-				"diff",
-				"gitcommit",
-				"gitrebase",
-			},
-		})
-	end,
+	opts = {
+		auto_cmd = false,
+		override_editorconfig = true,
+		filetype_exclude = {
+			"diff",
+			"gitcommit",
+			"gitrebase",
+		},
+	},
 }
