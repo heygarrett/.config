d467ac664f344fc2e6bddd5cf1197499543322c8
Garrett Oreilly <hey@heygarrett.com>
2024-03-25 11:50:21 -0400

    refactor(todo-comments): use opts table and keys table

--- nvim/lua/my/plugins/todo-comments.lua
+++ nvim/lua/my/plugins/todo-comments.lua
@@ -3,24 +3,21 @@ local todo_comments = function() return require("todo-comments") end
 return {
 	"https://github.com/folke/todo-comments.nvim",
 	dependencies = "https://github.com/nvim-lua/plenary.nvim",
-	init = function()
-		vim.keymap.set(
-			"n",
+	lazy = false,
+	keys = {
+		{
 			"]t",
 			function() todo_comments().jump_next() end,
-			{ desc = "jump to next todo comment" }
-		)
-		vim.keymap.set(
-			"n",
+			mode = { "n" },
+		},
+		{
 			"[t",
 			function() todo_comments().jump_prev() end,
-			{ desc = "jump to previous todo comment" }
-		)
-	end,
-	config = function()
-		require("todo-comments").setup({
-			signs = false,
-			highlight = { after = "" },
-		})
-	end,
+			mode = { "n" },
+		},
+	},
+	opts = {
+		signs = false,
+		highlight = { after = "" },
+	},
 }
