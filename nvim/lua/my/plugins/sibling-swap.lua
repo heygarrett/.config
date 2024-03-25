508e9ab52f34f2ae5985e1158a9a82d31897cafc
Garrett Oreilly <hey@heygarrett.com>
2024-03-25 11:41:58 -0400

    refactor(sibling-swap): use opts table and keys table

--- nvim/lua/my/plugins/sibling-swap.lua
+++ nvim/lua/my/plugins/sibling-swap.lua
@@ -3,39 +3,29 @@ local sibling_swap = function() return require("sibling-swap") end
 return {
 	"https://github.com/Wansmer/sibling-swap.nvim",
 	lazy = true,
-	init = function()
+	keys = {
 		-- Swap forward and back
-		vim.keymap.set(
-			"n",
+		{
 			"<leader>f",
 			function() sibling_swap().swap_with_right() end,
-			{ desc = "sibling-swap: move item to the right" }
-		)
-		vim.keymap.set(
-			"n",
+			mode = { "n" },
+		},
+		{
 			"<leader>b",
 			function() sibling_swap().swap_with_left() end,
-			{ desc = "sibling-swap: move item to the left" }
-		)
+			mode = { "n" },
+		},
 		-- Swap and flip operands
-		vim.keymap.set(
-			"n",
+		{
 			"<leader>F",
 			function() sibling_swap().swap_with_right_with_opp() end,
-			{
-				desc = "sibling-swap: move item to the right and update operator",
-			}
-		)
-		vim.keymap.set(
-			"n",
+			mode = { "n" },
+		},
+		{
 			"<leader>B",
 			function() sibling_swap().swap_with_left_with_opp() end,
-			{ desc = "sibling-swap: move item to the left and update operator" }
-		)
-	end,
-	config = function()
-		sibling_swap().setup({
-			use_default_keymaps = false,
-		})
-	end,
+			mode = { "n" },
+		},
+	},
+	opts = { use_default_keymaps = false },
 }
