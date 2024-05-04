df52a7477e14d96869a015296b34fc9fb19180ef
Garrett Oreilly <garrett.oreilly@agilebits.com>
2024-05-04 17:58:25 -0400

    feat(nvim): replace FTerm.nvim with toggleterm.nvim

---
+++ nvim/lua/my/plugins/toggleterm.lua
@@ -0,0 +1,21 @@
+return {
+	"https://github.com/akinsho/toggleterm.nvim",
+	keys = {
+		{
+			"<leader>t",
+			function() vim.cmd.ToggleTerm() end,
+			mode = { "n" },
+		},
+	},
+	opts = {
+		direction = "float",
+		on_open = function(term)
+			local bufnr = vim.api.nvim_win_get_buf(term.window)
+			local buf_line_count = vim.api.nvim_buf_line_count(bufnr)
+			local last_visible_line = vim.fn.line("w$", term.window)
+			if last_visible_line == buf_line_count then
+				vim.cmd.startinsert()
+			end
+		end,
+	},
+}
