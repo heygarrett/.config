7774d4dba97dff576109cc217d52f311a831dbcc
Garrett Oreilly <garrett.oreilly@agilebits.com>
2024-05-29 14:48:11 -0400

    feat(fish): streamline ,review_worktrees command
    Insead of removing worktrees one by one, create a list of worktrees to remove
    and then remove all of the ones in the list.

--- fish/functions/,review_worktrees.fish
+++ fish/functions/,review_worktrees.fish
@@ -1,9 +1,22 @@
 function ,review_worktrees
+	set removal_list
 	for wt in $(git worktree list | string split --fields 1 " ")
 		echo $wt
 		read -fP "Remove? " remove; or return
-		if test "$remove" = y
-			git worktree remove "$wt"
+		if test $remove = y
+			set --append removal_list $wt
+		end
+	end
+	set list_count (count $removal_list)
+	echo -e "\nWorktrees to remove:"
+	for wt in $removal_list
+		echo -e "\t"$wt
+	end
+	read -fP "Remove worktrees in this list? " remove; or return
+	if test $remove = y
+		for index in (seq $list_count)
+			echo "Removing... ["$index"/"$list_count"]"
+			git worktree remove $removal_list[$index]
 		end
 	end
 end
