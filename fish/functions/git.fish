561af721f83adf6e5fe2b97abaa4b70c35489f63
Garrett Oreilly <garrett.oreilly@agilebits.com>
2024-05-31 14:25:31 -0400

    stytle(fish): format git.fish

--- fish/functions/git.fish
+++ fish/functions/git.fish
@@ -1,9 +1,9 @@
 function git
 	set index (contains --index -- "--force" $argv)
-	if contains "push" $argv; and test -n "$index"
+	if contains push $argv; and test -n "$index"
 		read -fP "Did you mean --force-with-lease? [Y/n] " lease; or return
-		if test "$lease" != "n"
-			set argv[$index] "--force-with-lease"
+		if test "$lease" != n
+			set argv[$index] --force-with-lease
 		end
 	end
 	command git $argv
