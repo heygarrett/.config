28f1a5d8e0f7c3d5a1cdf7c9f1beb2325c3b318e
Garrett Oreilly <garrett.oreilly@agilebits.com>
2024-06-04 13:53:26 -0400

    refactor(fish): use global path variables in config

--- fish/config.fish
+++ fish/config.fish
@@ -10,20 +10,20 @@ if status is-login
 	set --global --export EDITOR "nvim --cmd 'let g:launched_by_shell=1'"
 	set --global --export GHCUP_USE_XDG_DIRS 1
 	set --global --export GOPATH $XDG_DATA_HOME/go
-	set --global --export HOMEBREW_BUNDLE_FILE ~/.config/homebrew/Brewfile
+	set --global --export HOMEBREW_BUNDLE_FILE $XDG_CONFIG_HOME/homebrew/Brewfile
 	set --global --export HOMEBREW_NO_ANALYTICS 1
 	set --global --export MANPAGER "$EDITOR +Man!"
-	set --global --export OP_CONFIG_DIR ~/.config/op
+	set --global --export OP_CONFIG_DIR $XDG_CONFIG_HOME/op
 	set --global --export RUSTUP_HOME $XDG_DATA_HOME/rustup
 	set --global --export STACK_XDG 1
-	set --global --export STARSHIP_CONFIG ~/.config/starship/starship.toml
-	set --global --export TEALDEER_CONFIG_DIR ~/.config/tealdeer
+	set --global --export STARSHIP_CONFIG $XDG_CONFIG_HOME/starship/starship.toml
+	set --global --export TEALDEER_CONFIG_DIR $XDG_CONFIG_HOME/tealdeer
 	set --global --export TERMINFO $XDG_DATA_HOME/terminfo
 	set --global --export TERMINFO_DIRS $TERMINFO /usr/share/terminfo
 
 	# PATH
 	fish_add_path --global \
-		~/.local/bin \
+		$HOME/.local/bin \
 		$GOPATH/bin \
 		$CARGO_HOME/bin \
 		/opt/homebrew/bin \
@@ -34,12 +34,12 @@ if status is-login
 	set --global fish_vi_force_cursor true
 
 	# create XDG_RUNTIME_DIR
-	if test ! -d ~/.runtime
-		mkdir ~/.runtime 2>/dev/null
+	if test ! -d $HOME/.runtime
+		mkdir $HOME/.runtime 2>/dev/null
 		or echo "Password required to create ~/.runtime"
-		and sudo mkdir ~/.runtime
-		and sudo chown $USER ~/.runtime
-		chmod 0700 ~/.runtime
+		and sudo mkdir $HOME/.runtime
+		and sudo chown $USER $HOME/.runtime
+		chmod 0700 $HOME/.runtime
 	end
 
 	# install wezterm terminfo
@@ -56,14 +56,14 @@ if status is-login
 	end
 
 	# set up 1Password ssh agent
-	set --local op_ssh ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+	set --local op_ssh $HOME/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
 	if test -e "$op_ssh"
 		set -x SSH_AUTH_SOCK "$op_ssh"
 	end
 end
 
 # extensions
-source ~/.config/op/plugins.sh 2>/dev/null
+source $XDG_CONFIG_HOME/op/plugins.sh 2>/dev/null
 starship init fish | source
 direnv hook fish | source
 fzf --fish | source
