b72757bc0b8a281606346fea94d87189b0ffa0b5
Garrett Oreilly <hey@heygarrett.com>
2024-06-19 20:03:47 -0400

    refactor(nvim): remove <c-o> remap to <s-tab>

--- nvim/lua/my/settings/navigation.lua
+++ nvim/lua/my/settings/navigation.lua
@@ -1,11 +1,6 @@
-vim.keymap.set(
-	"n",
-	"<s-tab>",
-	"<c-o>",
-	{ desc = "go to older position in the jump list" }
-)
 vim.keymap.set("n", "j", "gj", { desc = "go down one display line" })
 vim.keymap.set("n", "k", "gk", { desc = "go up to one display line" })
+
 vim.keymap.set("i", "<cr>", function()
 	local pum_info = vim.fn.complete_info({ "mode", "selected" })
 	if pum_info.mode ~= "" and pum_info.selected == -1 then
