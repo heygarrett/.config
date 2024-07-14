9e750321c9241bc128a1fd7c41fb9745bb2ae846
Garrett <hey@heygarrett.com>
2024-07-14 02:10:29 -0400

    fix(fidget.nvim): turn off notification deduplication

--- nvim/lua/my/plugins/fidget.lua
+++ nvim/lua/my/plugins/fidget.lua
@@ -1,14 +1,23 @@
 return {
 	"https://github.com/j-hui/fidget.nvim",
-	opts = {
-		notification = {
-			override_vim_notify = true,
-			window = {
-				border = "single",
-				normal_hl = "",
-				winblend = 0,
-				zindex = 99,
+	config = function()
+		require("fidget").setup({
+			notification = {
+				override_vim_notify = true,
+				configs = {
+					default = vim.tbl_extend(
+						"force",
+						require("fidget.notification").default_config,
+						{ update_hook = false }
+					),
+				},
+				window = {
+					border = "single",
+					normal_hl = "",
+					winblend = 0,
+					zindex = 99,
+				},
 			},
-		},
-	},
+		})
+	end,
 }
