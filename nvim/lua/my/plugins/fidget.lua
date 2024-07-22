de5c7387c5035524719a9de304537e901080375e
Garrett <hey@heygarrett.com>
2024-07-22 19:41:16 -0400

    feat(fidget.nvim): suppress LSP messages in insert mode

--- nvim/lua/my/plugins/fidget.lua
+++ nvim/lua/my/plugins/fidget.lua
@@ -2,6 +2,7 @@ return {
 	"https://github.com/j-hui/fidget.nvim",
 	config = function()
 		require("fidget").setup({
+			progress = { suppress_on_insert = true },
 			notification = {
 				override_vim_notify = true,
 				configs = {
