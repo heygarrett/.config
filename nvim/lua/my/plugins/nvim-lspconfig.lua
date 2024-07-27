56f7d94824fd3751c799f08eddb28b1266abc046
Garrett <hey@heygarrett.com>
2024-07-27 10:32:17 -0400

    refactor(nvim): remove rust-analyzer from Mason list

--- nvim/lua/my/plugins/nvim-lspconfig.lua
+++ nvim/lua/my/plugins/nvim-lspconfig.lua
@@ -116,54 +116,6 @@ return {
 					end,
 				})
 			end,
-			rust_analyzer = function()
-				lspconfig["rust_analyzer"].setup({
-					capabilities = capabilities,
-					settings = {
-						["rust-analyzer"] = {
-							cargo = { targetDir = true },
-							check = {
-								command = "clippy",
-								extraArgs = {
-									"--",
-									"--warn=clippy::todo",
-								},
-							},
-							inlayHints = {
-								-- bindingModeHints = { enable = false },
-								-- chainingHints = { enable = true },
-								-- closingBraceHints = {
-								-- 	enable = true,
-								-- 	minLines = 25,
-								-- },
-								-- closureCaptureHints = { enable = false },
-								closureReturnTypeHints = { enable = "always" },
-								-- closureStyle = "impl_fn",
-								-- discriminantHints = { enable = "never" },
-								-- expressionAdjustmentHints = {
-								-- 	enable = "never",
-								-- 	hideOutsideUnsafe = false,
-								-- 	mode = "prefix",
-								-- },
-								-- implicitDrops = { enable = false },
-								lifetimeElisionHints = {
-									enable = "always",
-									useParameterNames = false,
-								},
-								-- maxLength = 25,
-								-- parameterHints = { enable = true },
-								-- rangeExclusiveHints = { enable = false },
-								-- renderColons = true,
-								-- typeHints = {
-								-- 	enable = true,
-								-- 	hideClosureInitialization = false,
-								-- 	hideNamedConstructor = false,
-								-- },
-							},
-						},
-					},
-				})
-			end,
 			tsserver = function()
 				lspconfig["tsserver"].setup({
 					capabilities = capabilities,
@@ -205,6 +157,52 @@ return {
 			on_attach = function(client)
 				client.server_capabilities.documentFormattingProvider = false
 			end,
+		})
+		lspconfig["rust_analyzer"].setup({
+			capabilities = capabilities,
+			settings = {
+				["rust-analyzer"] = {
+					cargo = { targetDir = true },
+					check = {
+						command = "clippy",
+						extraArgs = {
+							"--",
+							"--warn=clippy::todo",
+						},
+					},
+					inlayHints = {
+						-- bindingModeHints = { enable = false },
+						-- chainingHints = { enable = true },
+						-- closingBraceHints = {
+						-- 	enable = true,
+						-- 	minLines = 25,
+						-- },
+						-- closureCaptureHints = { enable = false },
+						closureReturnTypeHints = { enable = "always" },
+						-- closureStyle = "impl_fn",
+						-- discriminantHints = { enable = "never" },
+						-- expressionAdjustmentHints = {
+						-- 	enable = "never",
+						-- 	hideOutsideUnsafe = false,
+						-- 	mode = "prefix",
+						-- },
+						-- implicitDrops = { enable = false },
+						lifetimeElisionHints = {
+							enable = "always",
+							useParameterNames = false,
+						},
+						-- maxLength = 25,
+						-- parameterHints = { enable = true },
+						-- rangeExclusiveHints = { enable = false },
+						-- renderColons = true,
+						-- typeHints = {
+						-- 	enable = true,
+						-- 	hideClosureInitialization = false,
+						-- 	hideNamedConstructor = false,
+						-- },
+					},
+				},
+			},
 		})
 		lspconfig["sourcekit"].setup({
 			capabilities = capabilities,
