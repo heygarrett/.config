acd46cd27741d1c2fa5e3c993c91d10dc02cc42e
Garrett <hey@heygarrett.com>
2024-08-16 09:06:14 -0400

    refactor(nvim): move autocommands to their own file

--- nvim/lua/my/settings/init.lua
+++ nvim/lua/my/settings/init.lua
@@ -1,10 +1,11 @@
 require("my.settings.options")
-
 require("my.settings.lsp")
-require("my.settings.commands")
+
+require("my.settings.autocommands")
 require("my.settings.folding")
 require("my.settings.indentation")
 require("my.settings.navigation")
 require("my.settings.sessions")
 require("my.settings.statusline")
 require("my.settings.terminal")
+require("my.settings.user_commands")
