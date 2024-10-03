742ba35288012afd3855acda4367303f24fae69a
Garrett <garrett.oreilly@agilebits.com>
2024-10-03 10:24:46 -0400

    refactor(fish): use switch statements for user input in functions

--- fish/functions/,review_worktrees.fish
+++ fish/functions/,review_worktrees.fish
@@ -1,13 +1,18 @@
 function ,review_worktrees
-	set removal_list
+	set --local removal_list
 	for wt in $(git worktree list | string split --fields 1 " ")
 		echo $wt
-		read -fP "Remove? " remove; or return
-		if test "$remove" = y
-			set --append removal_list $wt
+		while read --function --prompt-str "Remove? [y/n] " remove; or return 1
+			switch $remove
+				case y Y
+					set --append removal_list $wt
+					break
+				case n N
+					break
+			end
 		end
 	end
-	set list_count (count $removal_list)
+	set --local list_count (count $removal_list)
 	if test "$list_count" -eq 0
 		return
 	end
@@ -15,11 +20,16 @@ function ,review_worktrees
 	for wt in $removal_list
 		echo -e "\t"$wt
 	end
-	read -fP "Remove worktrees in this list? " remove; or return
-	if test "$remove" = y
-		for index in (seq $list_count)
-			echo "Removing... ["$index"/"$list_count"]"
-			git worktree remove $removal_list[$index]
+	while read --function --prompt-str "Remove worktrees in this list? [y/n] " remove; or return 1
+		switch $remove
+			case y Y
+				for index in (seq $list_count)
+					echo "Removing... ["$index"/"$list_count"]"
+					git worktree remove $removal_list[$index]
+				end
+				break
+			case n N
+				break
 		end
 	end
 end
