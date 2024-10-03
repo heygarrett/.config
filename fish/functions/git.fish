316b6d38839b1a74b3be86858fd0fd91963efff2
Garrett <garrett.oreilly@agilebits.com>
2024-10-03 10:24:46 -0400

    refactor(fish): use switch statements for user input in functions

--- fish/functions/git.fish
+++ fish/functions/git.fish
@@ -1,9 +1,14 @@
 function git
-	set index (contains --index -- "--force" $argv)
+	set --local index (contains --index -- "--force" $argv)
 	if contains push $argv; and test -n "$index"
-		read -fP "Did you mean --force-with-lease? [Y/n] " lease; or return
-		if test "$lease" != n
-			set argv[$index] --force-with-lease
+		while read --function --prompt-str "Did you mean --force-with-lease? [Y/n] " lease; or return 1
+			switch $lease
+				case y Y ""
+					set argv[$index] --force-with-lease
+					break
+				case n N
+					break
+			end
 		end
 	end
 	command git $argv
