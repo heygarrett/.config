028d88e6d9c99f0a4a6211c7413deaaa7937cb73
Garrett <garrett.oreilly@agilebits.com>
2024-10-11 12:08:34 -0400

    refactor(fish): use --local instead of --function in git function

--- fish/functions/git.fish
+++ fish/functions/git.fish
@@ -1,7 +1,7 @@
 function git
 	set --local index (contains --index -- "--force" $argv)
 	if contains push $argv; and test -n "$index"
-		while read --function --prompt-str "Did you mean --force-with-lease? [Y/n] " lease; or return 1
+		while read --local --prompt-str "Did you mean --force-with-lease? [Y/n] " lease; or return 1
 			switch $lease
 				case y Y ""
 					set argv[$index] --force-with-lease
