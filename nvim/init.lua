34322c196d1acf76165f80e17b4556c9ced0058e
Garrett <hey@heygarrett.com>
2024-11-04 18:33:07 -0500

    feat(lazy.nvim): handle failed initialization gracefully

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,7 +2,7 @@ require("my.settings")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
-	vim.system({
+	local cmd_result = vim.system({
 		"git",
 		"clone",
 		"--filter=blob:none",
@@ -10,8 +10,21 @@ if not vim.uv.fs_stat(lazypath) then
 		"https://github.com/folke/lazy.nvim.git",
 		lazypath,
 	}):wait()
+
+	if cmd_result.code ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ cmd_result.stderr, "WarningMsg" },
+			{ "\nHint: use `nvim --clean` to launch Neovim without broken config" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
+
 vim.opt.runtimepath:prepend(lazypath)
+
 require("lazy").setup("my.plugins", {
 	defaults = {
 		lazy = false,
