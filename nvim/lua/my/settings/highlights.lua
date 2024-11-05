9373ddfa05d65b31faa9ddff782bce761b03fc04
Garrett <hey@heygarrett.com>
2024-11-04 19:27:09 -0500

    feat(nvim): switch to default Neovim colorscheme

---
+++ nvim/lua/my/settings/highlights.lua
@@ -0,0 +1,7 @@
+vim.cmd.highlight({
+	args = { "StatusLine", "guifg=NvimLightGrey2", "guibg=NvimDarkGrey3" },
+})
+
+vim.cmd.highlight({
+	args = { "StatusLineNC", "guifg=NvimLightGrey4", "guibg=NvimDarkGrey2" },
+})
