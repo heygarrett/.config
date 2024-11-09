e6ed80ea1d318eb655ce95e430d441634c036fcc
Garrett <garrett.oreilly@agilebits.com>
2024-11-09 15:20:09 -0500

    feat(nvim): use jsonc filetype for json files with comments

--- nvim/lua/my/settings/filetypes.lua
+++ nvim/lua/my/settings/filetypes.lua
@@ -14,3 +14,16 @@ vim.filetype.add({
 
 -- use bash parser for dotenv files
 vim.treesitter.language.register("bash", "dotenv")
+
+-- TODO: use treesitter to search for jsonc-style comments instead
+local group = vim.api.nvim_create_augroup("filetypes", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+	desc = "use jsonc for json with comments",
+	group = group,
+	pattern = "json",
+	callback = function()
+		if vim.fn.search([[\v%(^|\s)//]], "nw", 0, 500) ~= 0 then
+			vim.cmd.setlocal({ args = { "filetype=jsonc" } })
+		end
+	end,
+})
