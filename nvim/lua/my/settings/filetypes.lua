9999624eefa38d5578518e4667398e398bf5ea3d
Garrett <garrett.oreilly@agilebits.com>
2024-11-16 15:37:28 -0500

    fix(nvim): improve detection of json with comments

--- nvim/lua/my/settings/filetypes.lua
+++ nvim/lua/my/settings/filetypes.lua
@@ -17,17 +17,45 @@ vim.filetype.add({
 -- use bash parser for dotenv files
 vim.treesitter.language.register("bash", "dotenv")
 
--- TODO: use treesitter to search for jsonc-style comments instead
 local group = vim.api.nvim_create_augroup("filetypes", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
 	desc = "use jsonc for json with comments",
 	group = group,
 	pattern = "json",
-	callback = function()
-		if vim.fn.search([[\v%(^|\s)//]], "nw", 0, 500) ~= 0 then
-			vim.cmd.setlocal({
-				args = { "filetype=jsonc" },
-			})
-		end
+	callback = function(filetype_event_opts)
+		vim.api.nvim_create_autocmd("DiagnosticChanged", {
+			desc = "use jsonc for json with comments",
+			group = group,
+			buffer = filetype_event_opts.buf,
+			once = true,
+			callback = function(diagnostic_event_opts)
+				for _, diagnostic in ipairs(diagnostic_event_opts.data.diagnostics) do
+					if
+						diagnostic.bufnr == diagnostic_event_opts.buf
+						-- Comments are not permitted in JSON. [521]
+						and diagnostic.code == 521
+					then
+						vim.cmd.setlocal({
+							args = { "filetype=jsonc" },
+						})
+						local jsonls_client_id = next(vim.lsp.get_clients({
+							bufnr = diagnostic_event_opts.buf,
+							name = "jsonls",
+						}))
+						if jsonls_client_id then
+							vim.lsp.buf_detach_client(
+								diagnostic_event_opts.buf,
+								jsonls_client_id
+							)
+							vim.lsp.buf_attach_client(
+								diagnostic_event_opts.buf,
+								jsonls_client_id
+							)
+						end
+						break
+					end
+				end
+			end,
+		})
 	end,
 })
