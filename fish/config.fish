70b6e2ac73f8749a023bca11c0a4a17a8223695b
Garrett <hey@heygarrett.com>
2024-11-24 14:36:17 -0500

    refactor(fish): use quotes to more safely expand env variables

--- fish/config.fish
+++ fish/config.fish
@@ -1,36 +1,36 @@
-set --global --export XDG_CACHE_HOME $HOME/.cache
-set --global --export XDG_CONFIG_HOME $HOME/.config
-set --global --export XDG_DATA_HOME $HOME/.local/share
-set --global --export XDG_RUNTIME_DIR $HOME/.runtime
-set --global --export XDG_STATE_HOME $HOME/.local/state
+set --global --export XDG_CACHE_HOME "$HOME"/.cache
+set --global --export XDG_CONFIG_HOME "$HOME"/.config
+set --global --export XDG_DATA_HOME "$HOME"/.local/share
+set --global --export XDG_RUNTIME_DIR "$HOME"/.runtime
+set --global --export XDG_STATE_HOME "$HOME"/.local/state
 
 if status is-login
 	# environment variables
-	set --global --export CARGO_HOME $XDG_DATA_HOME/cargo
+	set --global --export CARGO_HOME "$XDG_DATA_HOME"/cargo
 	set --global --export DIRENV_WARN_TIMEOUT 0
 	set --global --export EDITOR "nvim --cmd 'let g:launched_by_shell=1'"
 	set --global --export GHCUP_USE_XDG_DIRS 1
-	set --global --export GOPATH $XDG_DATA_HOME/go
-	set --global --export HOMEBREW_BUNDLE_FILE $XDG_CONFIG_HOME/homebrew/Brewfile
+	set --global --export GOPATH "$XDG_DATA_HOME"/go
+	set --global --export HOMEBREW_BUNDLE_FILE "$XDG_CONFIG_HOME"/homebrew/Brewfile
 	set --global --export HOMEBREW_NO_ANALYTICS 1
-	set --global --export JJ_CONFIG $XDG_CONFIG_HOME/jj/config.toml
-	set --global --export MANPAGER "$EDITOR +Man!"
+	set --global --export JJ_CONFIG "$XDG_CONFIG_HOME"/jj/config.toml
+	set --global --export MANPAGER $EDITOR +Man!
 	set --global --export MISE_FISH_AUTO_ACTIVATE 0
-	set --global --export NPM_CONFIG_USERCONFIG $XDG_CONFIG_HOME/npm/npmrc
-	set --global --export OP_CONFIG_DIR $XDG_CONFIG_HOME/op
-	set --global --export RUSTUP_HOME $XDG_DATA_HOME/rustup
+	set --global --export NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME"/npm/npmrc
+	set --global --export OP_CONFIG_DIR "$XDG_CONFIG_HOME"/op
+	set --global --export RUSTUP_HOME "$XDG_DATA_HOME"/rustup
 	set --global --export STACK_XDG 1
-	set --global --export STARSHIP_CONFIG $XDG_CONFIG_HOME/starship/starship.toml
-	set --global --export TEALDEER_CONFIG_DIR $XDG_CONFIG_HOME/tealdeer
-	set --global --export TERMINFO $XDG_DATA_HOME/terminfo
+	set --global --export STARSHIP_CONFIG "$XDG_CONFIG_HOME"/starship/starship.toml
+	set --global --export TEALDEER_CONFIG_DIR "$XDG_CONFIG_HOME"/tealdeer
+	set --global --export TERMINFO "$XDG_DATA_HOME"/terminfo
 	set --global --export TERMINFO_DIRS $TERMINFO /usr/share/terminfo
 	set --global --export VISUAL $EDITOR
 
 	# PATH
 	fish_add_path --global \
-		$HOME/.local/bin \
-		$GOPATH/bin \
-		$CARGO_HOME/bin \
+		"$HOME"/.local/bin \
+		"$GOPATH"/bin \
+		"$CARGO_HOME"/bin \
 		/opt/homebrew/bin \
 		/opt/homebrew/sbin \
 		/opt/homebrew/opt/rustup/bin
@@ -63,7 +63,7 @@ if status is-login
 end
 
 # extensions
-source $XDG_CONFIG_HOME/op/plugins.sh 2>/dev/null
+source "$XDG_CONFIG_HOME"/op/plugins.sh 2>/dev/null
 starship init fish | source
 direnv hook fish | source
 fzf --fish | source
