e1509b8aca980f27e1258edae4c94d53b8eeaca7
Garrett <garrett.oreilly@agilebits.com>
2024-12-05 10:28:25 -0500

    refactor(oil.nvim): use init instead of config

--- nvim/lua/my/plugins/oil.lua
+++ nvim/lua/my/plugins/oil.lua
@@ -1,8 +1,18 @@
+local oil = function()
+	return require("oil")
+end
+
 return {
 	"https://github.com/stevearc/oil.nvim",
+	init = function()
+		vim.api.nvim_create_user_command("Ex", function()
+			oil().open()
+		end, { desc = "Open file browser" })
+	end,
 	opts = {
 		view_options = {
 			show_hidden = true,
+			---@param name string
 			is_always_hidden = function(name)
 				return name == ".."
 			end,
@@ -15,11 +25,4 @@ return {
 			["<C-v>"] = "actions.select_vsplit",
 		},
 	},
-	config = function(_, opts)
-		local oil = require("oil")
-		oil.setup(opts)
-		vim.api.nvim_create_user_command("Ex", function()
-			oil.open()
-		end, { desc = "Open file browser" })
-	end,
 }
