71a15a85bb39f624eb44b73b5368e17a0ac7b0f0
Garrett <hey@heygarrett.com>
2025-01-28 17:35:13 -0500

    feat(screenkey.nvim): center floating window

--- nvim/lua/my/plugins/screenkey.lua
+++ nvim/lua/my/plugins/screenkey.lua
@@ -2,7 +2,13 @@ return {
 	"https://github.com/NStefan002/screenkey.nvim",
 	cmd = "Screenkey",
 	opts = {
-		win_opts = { zindex = 99 },
+		win_opts = {
+			-- width is 40 by default
+			-- `col` is the right-most column of the floating window
+			-- adding 20 helps center it
+			col = math.ceil((vim.o.columns + 1) / 2) + 20,
+			zindex = 99,
+		},
 		group_mappings = true,
 		show_leader = true,
 	},
