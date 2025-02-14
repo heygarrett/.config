cff8ce34959c9e29be6b28437f996fcf40370302
Garrett <garrett.o@agilebits.com>
2025-02-14 12:09:51 -0500

    refactor(indentation.lua): simplify filetype check

--- nvim/lua/my/settings/indentation.lua
+++ nvim/lua/my/settings/indentation.lua
@@ -18,15 +18,15 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
 		-- override expandtab set by ftplugins
 		vim.bo.expandtab = vim.go.expandtab
 
-		-- detect indentation
-		local detected_indent = helpers.get_indentation_size(event_opts.buf)
-		if detected_indent ~= 0 and vim.bo.filetype ~= "gitcommit" then
-			vim.bo.expandtab = true
-			vim.bo.tabstop = detected_indent
-		end
+		if vim.bo.filetype ~= "gitcommit" then
+			-- detect indentation
+			local detected_indent = helpers.get_indentation_size(event_opts.buf)
+			if detected_indent ~= 0 then
+				vim.bo.expandtab = true
+				vim.bo.tabstop = detected_indent
+			end
 
-		-- load editorconfig
-		if vim.bo[event_opts.buf].filetype ~= "gitcommit" then
+			-- load editorconfig
 			require("editorconfig").config(event_opts.buf)
 		end
 
