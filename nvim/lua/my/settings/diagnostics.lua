4add6e0ceefb89149b4ea87f40589fbfd7a530df
Garrett <garrett.o@agilebits.com>
2025-05-02 21:03:39 -0400

    fix(nvim): standardize diagnostic message formatting

--- nvim/lua/my/settings/diagnostics.lua
+++ nvim/lua/my/settings/diagnostics.lua
@@ -1,5 +1,28 @@
+---@param diagnostic vim.Diagnostic
+---@return string?
+local format_diagnostic = function(diagnostic)
+	local components = { diagnostic.message }
+	if
+		diagnostic.source and not vim.startswith(diagnostic.message, diagnostic.source)
+	then
+		table.insert(components, 1, ("%s:"):format(diagnostic.source))
+	end
+	if diagnostic.code then
+		table.insert(components, ("[%s]"):format(diagnostic.code))
+	end
+
+	return table.concat(components, " ")
+end
+
 vim.diagnostic.config({
-	float = { source = true },
+	float = {
+		format = format_diagnostic,
+		suffix = "",
+	},
+	virtual_lines = {
+		current_line = true,
+		format = format_diagnostic,
+	},
 	signs = {
 		text = {
 			[vim.diagnostic.severity.ERROR] = "",
@@ -13,23 +36,6 @@ vim.diagnostic.config({
 			[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
 			[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
 		},
-	},
-	virtual_lines = {
-		current_line = true,
-		format = function(diagnostic)
-			local components = { diagnostic.message }
-			if
-				diagnostic.source
-				and not vim.startswith(diagnostic.message, diagnostic.source)
-			then
-				table.insert(components, 1, ("%s:"):format(diagnostic.source))
-			end
-			if diagnostic.code then
-				table.insert(components, ("[%s]"):format(diagnostic.code))
-			end
-
-			return table.concat(components, " ")
-		end,
 	},
 })
 
