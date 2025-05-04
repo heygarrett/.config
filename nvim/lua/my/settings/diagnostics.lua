b2f471ed32d965bf8e67479da1429e274a5cdbab
Garrett <hey@heygarrett.com>
2025-05-03 22:24:34 -0400

    refactor(nvim): remove redundant :Diagnostics user command
    Now that the fzf picker can be resumed with <leader>r the list of diagnostics can be
    reopend more easily

--- nvim/lua/my/settings/diagnostics.lua
+++ nvim/lua/my/settings/diagnostics.lua
@@ -38,14 +38,3 @@ vim.diagnostic.config({
 		},
 	},
 })
-
-vim.api.nvim_create_user_command("Diagnostics", function()
-	local ok, choice = pcall(vim.fn.confirm, "", "&Document\n&workspace")
-	if not ok then
-		return
-	elseif choice == 1 then
-		vim.diagnostic.setloclist()
-	elseif choice == 2 then
-		vim.diagnostic.setqflist()
-	end
-end, { desc = "add buffer diagonstics to location list" })
