26c6a6bbf180f14208915efa9f1c25cd8735882e
Garrett <garrett.o@agilebits.com>
2025-05-14 14:39:08 -0400

    fix(lsp): update rust-analyzer inlay hints with types

--- nvim/after/lsp/rust_analyzer.lua
+++ nvim/after/lsp/rust_analyzer.lua
@@ -11,24 +11,35 @@ return {
 					"--warn=clippy::todo",
 				},
 			},
+			-- https://github.com/rust-lang/rust-analyzer/blob/master/editors/code/package.json
 			inlayHints = {
 				-- bindingModeHints = { enable = false },
-				-- chainingHints = { enable = true },
+				chainingHints = { enable = false },
 				-- closingBraceHints = {
 				-- 	enable = true,
 				-- 	minLines = 25,
 				-- },
 				-- closureCaptureHints = { enable = false },
-				closureReturnTypeHints = { enable = "always" },
+				closureReturnTypeHints = {
+					---@type "always" | "never" | "with_block"
+					enable = "with_block",
+				},
+				-- ---@type "impl_fn" | "rust_analyzer" | "with_id" | "hide"
 				-- closureStyle = "impl_fn",
-				-- discriminantHints = { enable = "never" },
+				-- discriminantHints = {
+				-- 	---@type "always" | "never" | "fieldless"
+				-- 	enable = "never",
+				-- },
 				-- expressionAdjustmentHints = {
+				-- 	---@type "always" | "never" | "reborrow"
 				-- 	enable = "never",
 				-- 	hideOutsideUnsafe = false,
+				-- 	---@type "prefix" | "postfix" | "prefer_prefix" | "prefer_postfix"
 				-- 	mode = "prefix",
 				-- },
 				-- implicitDrops = { enable = false },
 				lifetimeElisionHints = {
+					---@type "always" | "never" | "skip_trivial"
 					enable = "always",
 					useParameterNames = false,
 				},
