9cb562cb85506f5997a71fad34fb97bbb3e9b1ee
Garrett <garrett.o@agilebits.com>
2025-06-02 16:41:32 -0400

    refactor(fish): use long flags in keybindings function

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -2,7 +2,7 @@ function fish_user_key_bindings
 	fish_vi_key_bindings
 
 	bind \ce edit_command_buffer
-	bind -M insert \ce edit_command_buffer
-	bind -M insert \cf forward-bigword
-	bind -M insert \cy accept-autosuggestion
+	bind --mode insert \ce edit_command_buffer
+	bind --mode insert \cf forward-bigword
+	bind --mode insert \cy accept-autosuggestion
 end
