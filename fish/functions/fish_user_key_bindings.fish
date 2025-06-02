9abf16e31993ba59a6b14d37420b0e4601f605c9
Garrett <garrett.o@agilebits.com>
2025-06-02 16:58:26 -0400

    feat(fish): add fzf keybind for jj bookmarks

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,7 @@
 function fish_user_key_bindings
 	fish_vi_key_bindings
 
+	bind --mode insert \cb fzf-jj-bookmarks
 	bind \ce edit_command_buffer
 	bind --mode insert \ce edit_command_buffer
 	bind --mode insert \cf forward-bigword
