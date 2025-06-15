b285c2680be4f765c592c930ca04fe5c61d44aae
Garrett <hey@heygarrett.com>
2025-06-15 11:48:43 -0400

    feat(fish): keep jj bookmark colors in fzf selector

--- fish/functions/fzf-jj-bookmarks.fish
+++ fish/functions/fzf-jj-bookmarks.fish
@@ -1,6 +1,9 @@
 function fzf-jj-bookmarks
-	set --local selected_bookmark \
-		(jj bookmark list | grep --invert-match "^\s\+@" | fzf --reverse --height=~100%)
+	set --local selected_bookmark (
+		jj --color=always bookmark list |
+		grep --invert-match "^\s\+" |
+		fzf --ansi --reverse --height=~100%
+	)
 	if test -n "$selected_bookmark"
 		set --local bookmark_name (string split --max 1 --fields 1 ":" $selected_bookmark)
 		commandline --insert $bookmark_name
