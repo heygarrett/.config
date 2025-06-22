736cb3a00c648a042ab0d34e3092e40ed252e9b9
Garrett <hey@heygarrett.com>
2025-06-22 00:09:06 -0400

    refactor(nvim): remove helpers file

--- nvim/lua/my/settings/indentation.lua
+++ nvim/lua/my/settings/indentation.lua
@@ -1,14 +1,47 @@
-local helpers = require("my.helpers")
-
 -- Default to tabs
 vim.o.expandtab = false
 vim.o.shiftwidth = 0
 vim.o.softtabstop = 0
 vim.o.tabstop = 4
 vim.opt.listchars = { nbsp = "_", space = "·", tab = "| " }
-
 -- don't run editorconfig automatically
 vim.g.editorconfig = false
+
+---@param bufnr? integer
+---@return integer -- number of spaces or 0 for tabs
+local get_indentation_size = function(bufnr)
+	bufnr = bufnr or 0
+
+	local last_line = math.min(vim.api.nvim_buf_line_count(bufnr), 1000)
+
+	---@type string[]
+	local leading_whitespace = {}
+	for _, line in ipairs(vim.api.nvim_buf_get_lines(bufnr, 0, last_line, true)) do
+		table.insert(leading_whitespace, line:match("^%s+"))
+	end
+
+	if vim.tbl_isempty(leading_whitespace) then
+		return 0
+	end
+
+	local tab_count = 0
+	---@type string[]
+	local leading_spaces = {}
+	for _, w in ipairs(leading_whitespace) do
+		if w:find("\t") then
+			tab_count = tab_count + 1
+		elseif #w > 1 then -- ignore single-space indents
+			table.insert(leading_spaces, w)
+		end
+	end
+	if tab_count >= #leading_spaces then
+		return 0
+	end
+
+	table.sort(leading_spaces)
+
+	return #leading_spaces[1]
+end
 
 local group = vim.api.nvim_create_augroup("indentation", { clear = true })
 vim.api.nvim_create_autocmd("BufWinEnter", {
@@ -20,7 +53,7 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
 
 		if vim.bo.filetype ~= "gitcommit" then
 			-- detect indentation
-			local detected_indent = helpers.get_indentation_size(event_opts.buf)
+			local detected_indent = get_indentation_size(event_opts.buf)
 			if detected_indent ~= 0 then
 				vim.bo.expandtab = true
 				vim.bo.tabstop = detected_indent
@@ -38,7 +71,7 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
 			event_opts.buf,
 			"Retab",
 			function(command_opts)
-				local current_indent = helpers.get_indentation_size(event_opts.buf)
+				local current_indent = get_indentation_size(event_opts.buf)
 				if (current_indent ~= 0) == vim.bo.expandtab then
 					return
 				end
