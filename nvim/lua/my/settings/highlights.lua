42be2c15ca9dfc05b5b36704e8a75b12f9c14feb
Garrett <garrett.o@agilebits.com>
2025-07-08 10:39:00 -0400

    refactor(highlights.lua): sort highlights

--- nvim/lua/my/settings/highlights.lua
+++ nvim/lua/my/settings/highlights.lua
@@ -1,11 +1,11 @@
 vim.cmd.highlight({
+	args = { "DiffChange", "guifg=NvimLightGrey1", "guibg=NvimDarkGrey3" },
+})
+
+vim.cmd.highlight({
 	args = { "StatusLine", "gui=bold", "guifg=NvimLightGrey3", "guibg=NvimDarkGrey1" },
 })
 
 vim.cmd.highlight({
 	args = { "StatusLineNC", "guifg=NvimLightGrey4", "guibg=NvimDarkGrey1" },
-})
-
-vim.cmd.highlight({
-	args = { "DiffChange", "guifg=NvimLightGrey1", "guibg=NvimDarkGrey3" },
 })
