6a3abe729cda63364571ded4991c28354a93a2dc
Garrett <garrett.o@agilebits.com>
2025-07-22 13:20:31 -0400

    refactor(nvim): use `main` branch of treesitter plugins

--- nvim/lua/my/plugins/nvim-treesitter.lua
+++ nvim/lua/my/plugins/nvim-treesitter.lua
@@ -1,42 +1,82 @@
+local treesitter = function()
+	return require("nvim-treesitter")
+end
+
 return {
-	"https://github.com/nvim-treesitter/nvim-treesitter",
-	lazy = false,
-	dependencies = { "https://github.com/nvim-treesitter/nvim-treesitter-textobjects" },
-	build = function()
-		vim.cmd.TSUpdate()
-	end,
-	opts = {
-		auto_install = true,
-		ensure_installed = { "comment", "markdown_inline" },
-		ignore_install = { "diff", "gitcommit", "git_rebase" },
-		highlight = { enable = true },
-		indent = { enable = true },
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "<leader>ss",
-				node_incremental = "<leader>sm",
-				node_decremental = "<leader>sr",
-			},
-		},
-		textobjects = {
+	{
+		"https://github.com/nvim-treesitter/nvim-treesitter",
+		lazy = false,
+		branch = "main",
+		build = function()
+			vim.cmd.TSUpdate()
+		end,
+		init = function()
+			local ensure_installed = { "comment", "markdown_inline" }
+			local needed_parsers = vim.iter(ensure_installed)
+				:filter(function(parser)
+					return not vim.tbl_contains(treesitter().get_installed(), parser)
+				end)
+				:totable()
+			if not vim.tbl_isempty(needed_parsers) then
+				treesitter().install(needed_parsers)
+			end
+
+			local group = vim.api.nvim_create_augroup("treesitter", { clear = true })
+			vim.api.nvim_create_autocmd("FileType", {
+				desc = "enable treesitter",
+				group = group,
+				callback = function(event_opts)
+					local buf_parser = vim.treesitter.language.get_lang(event_opts.match)
+					if not buf_parser then
+						return
+					end
+
+					local ignore_install = { "diff", "gitcommit", "git_rebase" }
+					if vim.tbl_contains(ignore_install, buf_parser) then
+						return
+					end
+
+					-- TODO: find a way to wait for the parser to install
+					-- before enabling treesitter
+					if vim.treesitter.language.add(buf_parser) then
+						vim.treesitter.start(event_opts.buf, buf_parser)
+					else
+						if vim.tbl_contains(treesitter().get_available(), buf_parser) then
+							treesitter().install(buf_parser)
+						end
+					end
+				end,
+			})
+		end,
+	},
+	{
+		"https://github.com/nvim-treesitter/nvim-treesitter-textobjects",
+		branch = "main",
+		init = function()
+			local keymaps = {
+				["al"] = "@loop.outer",
+				["il"] = "@loop.inner",
+				["aC"] = "@class.outer",
+				["iC"] = "@class.inner",
+				["af"] = "@function.outer",
+				["if"] = "@function.inner",
+				["ac"] = "@conditional.outer",
+				["ic"] = "@conditional.inner",
+			}
+
+			for keys, caputure_group in pairs(keymaps) do
+				vim.keymap.set({ "x", "o" }, keys, function()
+					require("nvim-treesitter-textobjects.select").select_textobject(
+						caputure_group,
+						"textobjects"
+					)
+				end)
+			end
+		end,
+		opts = {
 			select = {
-				enable = true,
 				lookahead = true,
-				keymaps = {
-					["al"] = "@loop.outer",
-					["il"] = "@loop.inner",
-					["aC"] = "@class.outer",
-					["iC"] = "@class.inner",
-					["af"] = "@function.outer",
-					["if"] = "@function.inner",
-					["ac"] = "@conditional.outer",
-					["ic"] = "@conditional.inner",
-				},
 			},
 		},
 	},
-	config = function(_, opts)
-		require("nvim-treesitter.configs").setup(opts)
-	end,
 }
