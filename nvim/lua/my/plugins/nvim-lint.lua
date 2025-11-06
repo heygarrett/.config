844971f325551b5f11bf9e73e6fe907849cb546c
Garrett <garrett@iusevimbtw.com>
2025-11-06 00:21:48 -0500

    fix(nvim-lint): resolve linters per buffer instead of at plugin load

--- nvim/lua/my/plugins/nvim-lint.lua
+++ nvim/lua/my/plugins/nvim-lint.lua
@@ -2,8 +2,9 @@ local nvim_lint = function()
 	return require("lint")
 end
 
+---@param _ integer
 ---@return string?
-local commitlint = function()
+local commitlint = function(_)
 	local config_exists = next(vim.fs.find(function(name)
 		return name:match("commitlint") ~= nil
 	end, { upward = true, limit = 1 }))
@@ -16,6 +17,26 @@ local linters_by_filetype = {
 	jjdescription = { commitlint },
 }
 
+---@param bufnr integer
+---@return string[]
+local get_linters = function(bufnr)
+	local buf_filetype = vim.bo[bufnr].filetype
+	local filetype_linters = linters_by_filetype[buf_filetype]
+
+	return vim.iter(filetype_linters)
+		:map(
+			---@param linter string | function
+			function(linter)
+				if type(linter) == "function" then
+					return linter(bufnr)
+				else
+					return linter
+				end
+			end
+		)
+		:totable()
+end
+
 return {
 	"https://github.com/mfussenegger/nvim-lint",
 	init = function()
@@ -24,7 +45,7 @@ return {
 			desc = "nvim-lint",
 			group = group,
 			pattern = vim.tbl_keys(linters_by_filetype),
-			callback = function(event_opts)
+			callback = function(filetype_event_opts)
 				vim.api.nvim_create_autocmd({
 					"BufWinEnter",
 					"BufWritePost",
@@ -33,31 +54,15 @@ return {
 				}, {
 					desc = "nvim-lint",
 					group = group,
-					buffer = event_opts.buf,
-					callback = function()
-						nvim_lint().try_lint(nil, { ignore_errors = true })
+					buffer = filetype_event_opts.buf,
+					callback = function(lint_event_opts)
+						nvim_lint().try_lint(
+							get_linters(lint_event_opts.buf),
+							{ ignore_errors = true }
+						)
 					end,
 				})
 			end,
 		})
-	end,
-	config = function()
-		for filetype, linters in pairs(linters_by_filetype) do
-			---@type string[]
-			local resolved_linters = vim.iter(linters)
-				:map(
-					---@param linter string | function
-					function(linter)
-						if type(linter) == "function" then
-							return linter()
-						else
-							return linter
-						end
-					end
-				)
-				:totable()
-
-			nvim_lint().linters_by_ft[filetype] = resolved_linters
-		end
 	end,
 }
