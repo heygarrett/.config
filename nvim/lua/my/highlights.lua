5b5132d83ad9900f9dab9e1555808bcd72b0b340
Garrett <garrett@iusevimbtw.com>
2026-06-08 12:06:02 -0400

    feat(nvim): use minimal, intentional syntax highlights
    https://tonsky.me/blog/syntax-highlighting/

--- nvim/lua/my/highlights.lua
+++ nvim/lua/my/highlights.lua
@@ -32,3 +32,48 @@ vim.api.nvim_set_hl(0, "NonText", {
 	update = true,
 	nocombine = true,
 })
+
+-- clear syntax highlights
+for _, group in ipairs(vim.fn.getcompletion("@", "highlight")) do
+	local keeping = {
+		"comment",
+	}
+	for _, pattern in ipairs(keeping) do
+		if group:match(pattern) then
+			goto continue
+		end
+	end
+
+	vim.api.nvim_set_hl(0, group, {})
+
+	::continue::
+end
+
+-- intentional syntax highlights
+-- https://tonsky.me/blog/syntax-highlighting/
+vim.api.nvim_set_hl(0, "Comment", {
+	italic = true,
+	update = true,
+})
+vim.api.nvim_set_hl(0, "@comment", { link = "Comment" })
+
+vim.api.nvim_set_hl(0, "Constant", { fg = "NvimLightMagenta" })
+vim.api.nvim_set_hl(0, "@boolean", { link = "Constant" })
+vim.api.nvim_set_hl(0, "@character", { link = "Constant" })
+vim.api.nvim_set_hl(0, "@number", { link = "Constant" })
+
+vim.api.nvim_set_hl(0, "Declaration", { fg = "NvimLightBlue" })
+vim.api.nvim_set_hl(0, "@lsp.mod.declaration", { link = "Declaration" })
+
+vim.api.nvim_set_hl(0, "String", { fg = "NvimLightGreen" })
+vim.api.nvim_set_hl(0, "@string", { link = "String" })
+
+vim.api.nvim_set_hl(0, "Symbol", {
+	fg = "NvimLightGrey3",
+	update = true,
+})
+vim.api.nvim_set_hl(0, "@punctuation", { link = "Symbol" })
+vim.api.nvim_set_hl(0, "@operator", { link = "Symbol" })
+
+-- HTML
+vim.api.nvim_set_hl(0, "@none.html", { fg = "NvimLightBlue" })
