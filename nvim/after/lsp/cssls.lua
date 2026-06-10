e4531426b8b2e83b3ea59698693efcc8be2997d8
Garrett <garrett@iusevimbtw.com>
2026-06-10 19:00:18 -0400

    fix(lsp): support `triggerSuggest` LSP command for cssls

--- nvim/after/lsp/cssls.lua
+++ nvim/after/lsp/cssls.lua
@@ -2,5 +2,10 @@
 return {
 	on_attach = function(client)
 		client.server_capabilities.documentFormattingProvider = false
+		-- after choosing a CSS property completion item the language server calls this
+		-- command, trying to trigger completion again for the property value
+		vim.lsp.commands["editor.action.triggerSuggest"] = function()
+			vim.lsp.completion.get()
+		end
 	end,
 }
