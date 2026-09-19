06c62f996fcfb55a7c43754a3755186048ea5024
Garrett <git@heygarrett.com>
2026-09-19 13:21:24 -0400

    fix(wezterm): avoid macOS accent selector when holding down a key

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -16,6 +16,9 @@ return {
 	tab_max_width = 1000,
 	use_fancy_tab_bar = false,
 
+	-- avoid accent selector in macOS when holding a key
+	use_ime = false,
+
 	-- HACK: prevent crashes
 	-- https://github.com/wezterm/wezterm/issues/7291#issuecomment-3404387484
 	front_end = "WebGpu",
